c6850473c2f4c43d1002403076d7d25b902dca1e
adrian <adrian@onlinemeded.org>
2019-09-13 13:58:01 -0500

    added a function to clean local branches that are not in the remote

--- oh-my-zsh/custom/rcfiles/functions/git.zsh
+++ oh-my-zsh/custom/rcfiles/functions/git.zsh
@@ -6,3 +6,7 @@ function g {
     git status --short --branch
   fi
 }
+
+function clean_local_branches () {
+  git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done
+}
