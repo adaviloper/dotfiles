f0c813de957bb600b3927317218782254bbc6af0
adrian <16654653+adaviloper@users.noreply.github.com>
2021-11-30 12:09:28 -0600

    updated php version number in path

--- oh-my-zsh/custom/rcfiles/path.zsh
+++ oh-my-zsh/custom/rcfiles/path.zsh
@@ -1,5 +1,5 @@
-export PATH="/usr/local/opt/php@7.2/bin:$PATH"
-export PATH="/usr/local/opt/php@7.2/sbin:$PATH"
+export PATH="/usr/local/opt/php@8.0/bin:$PATH"
+export PATH="/usr/local/opt/php@8.0/sbin:$PATH"
 export PATH="$PATH:/Applications/XAMPP/bin;yarn global bin;"
 export PATH="/Applications/PhpStorm.app/Contents/bin:$PATH"
 
