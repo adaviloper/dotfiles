fcf7709531ec77f9308728a749775943d54f186a
adrian <16654653+adaviloper@users.noreply.github.com>
2021-12-09 15:29:34 -0600

    added path export

--- oh-my-zsh/custom/rcfiles/path.zsh
+++ oh-my-zsh/custom/rcfiles/path.zsh
@@ -2,4 +2,4 @@ export PATH="/usr/local/opt/php@8.0/bin:$PATH"
 export PATH="/usr/local/opt/php@8.0/sbin:$PATH"
 export PATH="$PATH:/Applications/XAMPP/bin;yarn global bin;"
 export PATH="/Applications/PhpStorm.app/Contents/bin:$PATH"
-
+export PATH="/usr/local/opt/avr-gcc@7/bin:$PATH"
