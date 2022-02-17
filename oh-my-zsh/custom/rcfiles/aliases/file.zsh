2b2672e4f35c70fe3c9b7940f0ddf94640cf4856
adrian <16654653+adaviloper@users.noreply.github.com>
2022-02-17 14:45:53 -0600

    updates

--- oh-my-zsh/custom/rcfiles/aliases/file.zsh
+++ oh-my-zsh/custom/rcfiles/aliases/file.zsh
@@ -28,8 +28,3 @@ alias serve='serve-laravel'
 
 # Path updates
 export PATH=/Applications/PhpStorm.app/Contents/bin:$PATH
-
-# servers Connections
-alias holy-farm="ssh forge@96.126.124.75"
-alias app1="ssh forge@54.214.53.168"
-alias app2="ssh forge@35.162.21.101"
