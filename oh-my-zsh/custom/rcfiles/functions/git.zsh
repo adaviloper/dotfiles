97fe6cd967d17e7895941114fdc65bcc77c7f35e
adrian <16654653+adaviloper@users.noreply.github.com>
2022-02-17 09:33:51 -0600

    added fuzzy checkout function

--- oh-my-zsh/custom/rcfiles/functions/git.zsh
+++ oh-my-zsh/custom/rcfiles/functions/git.zsh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 compdef g=git
+
 function g {
   if [[ $# -gt 0 ]]; then
     git "$@"
@@ -9,4 +12,21 @@ function g {
 
 function clean_local_branches () {
   git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done
+}
+
+function fuzzy_co () {
+  grepped_branches=$(git branch | grep "$1" | sed 's/*//')
+  eval "branches=($grepped_branches)"
+  branch_count=${#branches[*]}
+  if (( $branch_count == 0 )); then
+    echo "No matching branches found"
+  elif (( $branch_count == 1 )); then
+    gco "${branches[1]}"
+  else
+    select branch in $branches
+    do
+      gco $branch
+      break
+    done
+  fi
 }
