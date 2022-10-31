d9e951114a6d4a36d549ac96e6f9f3b6015eeaf7
adrian <16654653+adaviloper@users.noreply.github.com>
2022-10-31 18:41:04 -0500

    updated things

--- oh-my-zsh/custom/rcfiles/functions/git.zsh
+++ oh-my-zsh/custom/rcfiles/functions/git.zsh
@@ -14,24 +14,30 @@ function clean_local_branches () {
   git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done
 }
 
-function gco () {
-  if [[ "$1" == "-" ]]; then
-    git checkout $1
-    return
-  fi
+function select_from_matching_branches()
+{
   grepped_branches=$(git branch ${@:2} | grep "$1" | sed 's/*//')
   eval "branches=($grepped_branches)"
   branch_count=${#branches[*]}
   if (( $branch_count == 0 )); then
-    echo "No matching branches found"
-    git checkout $1
+    echo $1
   elif (( $branch_count == 1 )); then
-    gco "${branches[1]}"
+    echo "${branches[1]}"
   else
     select branch in $branches
     do
-      gco $branch
+      echo $branch
       break
     done
+  fi
+}
+
+function gco () {
+  if [[ "$1" == "-" ]]; then
+    git checkout $1
+    return
+  else
+    local branch=$(select_from_matching_branches $1)
+    gco $branch
   fi
 }
--- oh-my-zsh/custom/rcfiles/functions/grin.zsh
+++ oh-my-zsh/custom/rcfiles/functions/grin.zsh
@@ -8,6 +8,22 @@ function epic()
   ticket_branch 'epic' $1 $2
 }
 
+function merge()
+{
+  local branch=$(select_from_matching_branches $1)
+  gcb "merge/$(git_current_branch)_WITH_${branch}_$(date +"%Y-%m-%d")"
+}
+
+function revert()
+{
+  ticket_branch 'revert' $1 $2
+}
+
+function spike()
+{
+  ticket_branch 'spike' $1 $2
+}
+
 function story()
 {
   ticket_branch 'story' $1 $2
@@ -16,11 +32,6 @@ function story()
 function task()
 {
   ticket_branch 'task' $1 $2
-}
-
-function revert()
-{
-  ticket_branch 'revert' $1 $2
 }
 
 function ticket_branch()
