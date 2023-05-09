c86c111855104c21e4864f2277ce094e6436cdaa
adrian <16654653+adaviloper@users.noreply.github.com>
2023-05-08 23:12:47 -0500

    added colors and updated neovim statusline

--- config/nvim/lua/user/plugins/heirline.lua
+++ config/nvim/lua/user/plugins/heirline.lua
@@ -5,9 +5,52 @@ return {
     opts.statusline = {
       -- statusline
       hl = { fg = "fg", bg = "bg" },
-      status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
-      status.component.git_branch(),
-      status.component.file_info { filetype = {}, filename = false, file_modified = false },
+      status.component.mode {
+        -- enable mode text with padding as well as an icon before it
+        mode_text = {
+          icon = {
+            kind = "VimIcon",
+            padding = {
+              right = 1,
+            }
+          },
+          padding = {
+            right = 1,
+            left = 1
+          },
+        },
+        -- surround the component with a separators
+        surround = {
+          -- it's a left element, so use the left separator
+          separator = "left",
+          -- set the color of the surrounding based on the current mode using astronvim.utils.status module
+          color = function() return { main = status.hl.mode_bg(), right = "terminal" } end,
+        },
+      },
+      status.component.git_branch {
+        surround = {
+          separator = "left",
+          color = function() return { main = "terminal", right = "file_info_bg" } end,
+        },
+        padding = {
+          right = 1,
+          left = 1
+        },
+      },
+      status.component.file_info {
+        file_icon = {
+          padding = {
+            left = 1,
+            right = 1,
+          }
+        },
+        padding = {
+          right = 1,
+          left = 1
+        },
+        file_modified = false,
+        color = function() return { main = "file_info_bg", right = "git_diff_bg" } end,
+      },
       status.component.git_diff(),
       status.component.diagnostics(),
       status.component.fill(),
@@ -16,7 +59,6 @@ return {
       status.component.lsp(),
       status.component.treesitter(),
       status.component.nav(),
-      status.component.mode { surround = { separator = "right" } },
     }
 
     return opts
