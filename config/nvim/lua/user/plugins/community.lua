c9a5bd98bf28abc176c4ca1fd067c592ee41d6df
adrian <16654653+adaviloper@users.noreply.github.com>
2023-06-06 16:33:25 -0500

    switched to using community configs

--- config/nvim/lua/user/plugins/community.lua
+++ config/nvim/lua/user/plugins/community.lua
@@ -6,5 +6,8 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  -- { import = "astrocommunity.editing-support.refactoring-nvim" },
 }
--- config/nvim/lua/user/plugins/mason.lua
+++ config/nvim/lua/user/plugins/mason.lua
@@ -21,6 +21,24 @@ return {
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
       -- ensure_installed = { "python" },
+      handlers = {
+        php = function()
+          local dap = require "dap"
+          dap.adapters.php = {
+            type = "executable",
+            command = os.getenv "HOME" .. "/.local/share/nvim_astro/mason/bin/php-debug-adapter",
+          }
+
+          dap.configurations.php = {
+            {
+              type = "php",
+              request = "launch",
+              port = 9003,
+              name = "Listen for Xdebug",
+            },
+          }
+        end,
+      },
     },
   },
 }
