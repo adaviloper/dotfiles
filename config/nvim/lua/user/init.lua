8203a034abb086b82017ddab721bb2a5b1bf33a6
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-08-02 23:00:25 -0400

    addressing php intelliphense

--- config/nvim/lua/user/init.lua
+++ config/nvim/lua/user/init.lua
@@ -110,7 +110,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
+        enabled = false,     -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -130,6 +130,7 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+      "intelephense"
     },
   },
   -- Configure require("lazy").setup() options
--- config/nvim/lua/user/plugins/community.lua
+++ config/nvim/lua/user/plugins/community.lua
@@ -4,12 +4,10 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
-  { import = "astrocommunity.pack.php" },
   -- { import = "astrocommunity.editing-support.refactoring-nvim" },
 }
