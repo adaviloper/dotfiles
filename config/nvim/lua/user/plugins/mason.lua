a5ef56b44998cd5ec483060241da88a70052d22e
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-08-23 17:04:56 -0500

    updating lsp configs

--- config/nvim/lua/user/plugins/mason.lua
+++ config/nvim/lua/user/plugins/mason.lua
@@ -13,21 +13,78 @@ return {
       automatic_installation = true,
     },
     config = function ()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
       -- PHP
-      require('lspconfig').intelephense.setup({})
+      require('lspconfig').intelephense.setup({
+        commands = {
+          IntelephenseIndex = {
+            function ()
+              vim.lsp.buf.execute_comand({ command = 'intelephense.index.workspace' })
+            end
+          },
+        },
+        on_attach = function (client, bufnr)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+        capabilities = capabilities
+      })
+      require('lspconfig').phpactor.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          client.server_capabilities.completionProvider = false
+          client.server_capabilities.hoverProvider = false
+          client.server_capabilities.implementationProvider = false
+          client.server_capabilities.referencesProvider = false
+          client.server_capabilities.renameProvider = false
+          client.server_capabilities.selectionRangeProvider = false
+          client.server_capabilities.signatureHelpProvider = false
+          client.server_capabilities.typeDefinitionProvider = false
+          client.server_capabilities.workspaceSymbolProvider = false
+          client.server_capabilities.definitionProvider = false
+          client.server_capabilities.documentHighlightProvider = false
+          client.server_capabilities.documentSymbolProvider = false
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+        init_options = {
+          ["language_server_phpstan.enabled"] = false,
+          ["language_server_psalm.enabled"] = false,
+        },
+        handlers = {
+          ['textDocument/publishDiagnostics'] = function() end
+        }
+      })
 
       -- Vue, JavaScript, Typescript
       require('lspconfig').volar.setup({
-        -- Enable "Take Over Mode" where avolar will provide all JS/TS LSP services
+        on_attach = function(client, bufnr)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+          -- if client.server_capabilities.inlayHintProvider then
+          --   vim.lsp.buf.inlay_hint(bufnr, true)
+          -- end
+        end,
+        capabilities = capabilities,
+        -- Enable "Take Over Mode" where volar will provide all JS/TS LSP services
         -- This drastically improves the responsiveness of diagnostic updates on change
-        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
       })
 
       -- TailwindCSS
-      require('lspconfig').tailwindcss.setup({})
+      require('lspconfig').tailwindcss.setup({ capabilities = capabilities })
 
+      -- JSON
+      require('lspconfig').jsonls.setup({
+        capabilities = capabilities,
+        settings = {
+          json = {
+            schemas = require('schemastore').json.schemas(),
+          },
+        },
+      })
       -- Lua
-      require('lspconfig').lua_ls.setup({})
+      require('lspconfig').lua_ls.setup({ capabilities = capabilities })
     end
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
