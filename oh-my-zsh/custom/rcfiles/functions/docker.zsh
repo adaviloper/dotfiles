6741d04ca27db8eeef1a0c99497ee803b89f8c4a
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-08-29 10:45:47 -0500

    adding aliases for docker

---
+++ oh-my-zsh/custom/rcfiles/functions/docker.zsh
@@ -0,0 +1,23 @@
+alias dwhip="docker exec -it whiplash-app"
+alias dwhipart="docker exec -it whiplash-app php artisan"
+alias dwhiptest="docker exec -it whiplash-app ./vendor/bin/phpunit"
+
+# function dcore() {
+#   if [[ "$1" -e "artisan"]] then
+#     docker exec -it core php artisan
+#   fi
+#   if [[ "$1" -e "t"]] then
+#     docker exec -it core ./vendor/bin/phpunit
+#   fi
+#   if [[ "$1" -e "tf"]] then
+#     docker exec -it core ./vendor/bin/phpunit --filter $2
+#   fi
+# }
+#
+# function dart() {
+#   dcore artisan
+# }
+#
+# function dtf() {
+#   docker exec -it core ./vendor/bin/phpunit --filter "$1"
+# }
