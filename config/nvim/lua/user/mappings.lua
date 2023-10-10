44bd9c31822be4dd194c71261d7b10351b4e4785
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-10-09 20:29:59 -0500

    Removed get_icon referenced. I think this solved some circularity issue

--- config/nvim/lua/user/mappings.lua
+++ config/nvim/lua/user/mappings.lua
@@ -1,20 +1,11 @@
 local utils = require "user.utils"
-local astroUtils = require("astronvim.utils")
-local get_icon = astroUtils.get_icon
 
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
 return {
   -- first key is the mode
   n = {
-    -- second key is the lefthand side of the map
     ["<leader>W"] = { "<cmd>wa<cr>", desc = "Save all" },
+    -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
@@ -24,6 +15,11 @@ return {
       end,
       desc = "Pick to close",
     },
+    -- tables with the `name` key will be registered with which-key if it's installed
+    -- this is useful for naming menus
+    ["<leader>b"] = { name = "Buffers" },
+    -- quick save
+    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
     ["<leader>c"] = {
       function()
         local bufs = vim.fn.getbufinfo({ buflisted = true })
@@ -34,25 +30,18 @@ return {
       end,
       desc = "Close buffer",
     },
-    ["<leader>fs"] = { "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", desc = "Find a symbol in the workspace" },
     -- Harpoon
-    ["<leader>m"] = { name = get_icon('Harpoon', 1, true).."Harpoon" },
+    ["<leader>m"] = { name = "󱡅 Harpoon" },
     ["<leader>ma"] = { function () require("harpoon.mark").add_file() end, desc = "Add file to Harpoon list" },
     ["<leader>mm"] = { function () require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Harpoon quick menu" },
     ["<leader>ml"] = { function () require("harpoon.ui").nav_next() end, desc = "Navigate to previous harpoon mark" },
     ["<leader>mh"] = { function () require("harpoon.ui").nav_prev() end, desc = "Navigate to next harpoon mark" },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- better search
-    n = { utils.better_search "nzz", desc = "Next search" },
-    N = { utils.better_search "Nzz", desc = "Previous search" }, -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    X = { "x~", desc = "Delete current character and capitalize the next"},
     -- ISwap
     ["Q"] = { "<cmd>ISwapWith<cr>" },
     [">p"] = { "<cmd>ISwapWithRight<cr>", desc = "Swap node with right" },
-    ["<p"] = { "<cmd>ISwapWithLeft<cr>", desc = "Swap node with left" },
-    -- LuaSnip
-    ["<leader><leader>s"] = { "<cmd>source ~/.config/nvim/lua/user/plugins/luasnip.lua<cr>", desc = "Source LuaSnip snippet files" },
+    ["<p"] = { "<cmd>ISwapWithLeft<cr>", desc = "Swap node with left" },    -- better search
+    n = { utils.better_search "nzz", desc = "Next search" },
+    N = { utils.better_search "Nzz", desc = "Previous search" }, -- quick save
     -- NeoTeset
     ["<F4>"] = { name = "NeoTest", },
     ["<F4>n"] = { function() require("neotest").run.run() end, desc = "Run nearest test" },
@@ -62,12 +51,21 @@ return {
     ["<F4>a"] = { function() require("neotest").run.attach() end, desc = "Attach to the nearest test" },
     ["<F4>o"] = { function() require("nejotest").output_panel.toggle() end, desc = "View the output" },
     ["<F4>s"] = { function() require("neotest").summary.toggle() end, desc = "View the output" },
-    ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<cr>", desc = "Go to definition"},
     ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<cr>", desc = "Go to definition"},
     ["gr"] = { "<cmd>Telescope lsp_references<cr>", desc = "Go to references"},
+        -- Telescope
+    ["<leader>T"] = { name = "Telescope" },
+    ['<A-j>'] = { ':move .+1<CR>=='},
+    ['<A-k>'] = { ':move .-2<CR>=='},
+    ['<A-Down>'] = { '<cmd>resize -4<CR>' },
+    ['<A-Up>'] = { '<cmd>resize +4<CR>' },
+    ['<A-Left>'] = { '<cmd>vertical resize -4<CR>' },
+    ['<A-Right>'] = { '<cmd>vertical resize +4<CR>' },
+    X = { "x~", desc = "Delete current character and capitalize the next"},
   },
   i = {
-    --
+    ['<A-j>'] = { '<Esc>:move .+1<CR>==gi'},
+    ['<A-k>'] = { '<Esc>:move .-2<CR>==gi'},
   },
   v = {
     ["<"] = { "<gv", desc = "Unindent without losing selection"},
