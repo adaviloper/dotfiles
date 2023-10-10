8e380c99fcde39d94c1274e8c0c06bebc4ab7348
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-10-09 19:00:51 -0500

    Switched to bidirectional leaping

--- config/nvim/lua/user/plugins/leap.lua
+++ config/nvim/lua/user/plugins/leap.lua
@@ -3,5 +3,9 @@ return {
   lazy = false,
   config = function()
     require('leap').add_default_mappings()
+    vim.keymap.set({"n", "v"}, "s", function ()
+      local current_window = vim.fn.win_getid()
+      require('leap').leap { target_windows = { current_window } }
+    end)
   end
 }
