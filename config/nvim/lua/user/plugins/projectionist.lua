1f4fcaad6d25fd2f1cdf62c8a5f432898bcc93f8
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-10-15 14:09:56 -0500

    nvim config updates

--- config/nvim/lua/user/plugins/projectionist.lua
+++ config/nvim/lua/user/plugins/projectionist.lua
@@ -1,43 +1,65 @@
-vim.g.projectionist_heuristics = {
-  artisan = {
-    ['*'] = {
-      start = 'sail up',
-      console = 'sail tinker',
-      make = 'npm run dev',
-    },
-    ['app/Models/*.php'] = {
-      type = 'model',
-    },
-    ['app/Http/Controllers/*.php'] = {
-      type = 'controller',
-    },
-    ['routes/*.php'] = {
-      type = 'route',
-    },
-    ['database/migrations/*.php'] = {
-      type = 'migration',
-    },
-    ['app/*.php'] = {
-      type = 'source',
-      alternate = {
-        'tests/Unit/{}Test.php',
-        'tests/Feature/{}Test.php',
-      },
-    },
-    ['tests/Feature/*Test.php'] = {
-      type = 'test',
-      alternate = 'app/{}.php',
-    },
-    ['tests/Unit/*Test.php'] = {
-      type = 'test',
-      alternate = 'app/{}.php',
-    },
-  },
-}
 return {
   'tpope/vim-projectionist',
-  requires = 'tpope/vim-dispatch',
+  dependencies = 'tpope/vim-dispatch',
   config = function()
-    require('user.plugins.projectionist')
+    vim.g.projectionist_heuristics = {
+      artisan = {
+        ['*'] = {
+          start = 'php artisan serve',
+          console = 'php artisan tinker',
+        },
+        ['app/*.php'] = {
+          type = 'source',
+          alternate = {
+            'tests/Unit/{}Test.php',
+            'tests/Feature/{}Test.php',
+          },
+        },
+        ['tests/Feature/*Test.php'] = {
+          type = 'test',
+          alternate = 'app/{}.php',
+        },
+        ['tests/Unit/*Test.php'] = {
+          type = 'test',
+          alternate = 'app/{}.php',
+        },
+        ['app/Models/*.php'] = {
+          type = 'model',
+        },
+        ['app/Http/Controllers/*.php'] = {
+          type = 'controller',
+        },
+        ['routes/*.php'] = {
+          type = 'route',
+        },
+        ['database/migrations/*.php'] = {
+          type = 'migration',
+        },
+      },
+      ['src/&composer.json'] = {
+        ['src/*.php'] = {
+          type = 'source',
+          alternate = {
+            'tests/{}Test.php',
+          },
+        },
+        ['tests/*Test.php'] = {
+          type = 'test',
+          alternate = 'src/{}.php',
+        },
+      },
+      ['app/&composer.json'] = {
+        ['app/*.php'] = {
+          type = 'source',
+          alternate = {
+            'tests/{}Test.php',
+          },
+        },
+        ['tests/*Test.php'] = {
+          type = 'test',
+          alternate = 'app/{}.php',
+        },
+      },
+    }
   end,
 }
