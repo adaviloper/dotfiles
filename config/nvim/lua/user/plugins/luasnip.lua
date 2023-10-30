2de27b1f04a4d3954da188f7933f47c38d2517ec
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-10-30 11:49:20 -0500

    switching to LuaSnip snippets

--- config/nvim/lua/user/plugins/luasnip.lua
+++ config/nvim/lua/user/plugins/luasnip.lua
@@ -2,9 +2,11 @@ return {
   "L3MON4D3/LuaSnip",
   enabled = true,
   config = function(plugin, opts)
+    local ls = require("luasnip")
     require "plugins.configs.luasnip" (plugin, opts)                                       -- include the default astronvim config that calls the setup call
-    require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
-    -- opts.history = true
+    
+    ls.setup()
+    require("luasnip.loaders.from_lua").load({ paths = { "./lua/user/snippets" } }) -- load snippets paths
     return opts
   end,
 }
---
+++ config/nvim/lua/user/snippets/php.lua
@@ -0,0 +1,21 @@
+return {
+  s('pubf', 
+    {
+      fmt({
+        "public function (<>): <>",
+        "{",
+        "    <>",
+        "}",
+      },
+        {
+          i(1),
+          i(2),
+          i(3),
+        }
+      ),
+    }
+  )
+}, 
+  {
+    s('autotest', t('this is an auto test'))
+  }
