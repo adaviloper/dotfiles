79f9cfa77598a0e0739aa353182e6ea72100a920
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-11-08 17:13:05 -0600

    moving gd definition

--- config/nvim/lua/user/lsp/mappings.lua
+++ config/nvim/lua/user/lsp/mappings.lua
@@ -2,5 +2,11 @@ return {
   n = {
     -- this mapping will only be set in buffers with an LSP attached
     K = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
+    -- ["gd"] = {
+    --   function ()
+    --     require('laravel-route-symbol')
+    --   end,
+    --   desc = "Go to definition",
+    -- },
   },
 }
--- config/nvim/lua/user/mappings.lua
+++ config/nvim/lua/user/mappings.lua
@@ -54,7 +54,6 @@ return {
     ["<F4>a"] = { function() require("neotest").run.attach() end, desc = "Attach to the nearest test" },
     ["<F4>o"] = { function() require("nejotest").output_panel.toggle() end, desc = "View the output" },
     ["<F4>s"] = { function() require("neotest").summary.toggle() end, desc = "View the output" },
-    ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<cr>", desc = "Go to definition"},
     ["gr"] = { "<cmd>Telescope lsp_references<cr>", desc = "Go to references"},
         -- Telescope
     ['<leader>T'] = { name = 'Telescope' },
