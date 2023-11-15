8b5ccd4c4169c3ad2e2ee4c5d1f8fd69316eeb01
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-11-15 15:38:41 -0600

    testing sessions

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -18,6 +18,8 @@ return {
     autopairs_enabled = true,        -- enable autopairs at start
     diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    inlay_hints_enabled = true,
+    resession_enabled = true,
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
 }
