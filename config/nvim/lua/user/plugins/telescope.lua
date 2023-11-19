cd025c8be9dfeeb2cb445b133ff0fe104573b6b0
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-11-18 19:54:55 -0600

    ignore .git directory, but include hidden files in telescope

--- config/nvim/lua/user/plugins/telescope.lua
+++ config/nvim/lua/user/plugins/telescope.lua
@@ -6,16 +6,46 @@ return {
   -- the first parameter is the plugin specification
   -- the second is the table of options as set up in Lazy with the `opts` key
   config = function(plugin, opts)
-    -- run the core AstroNvim configuration function with the options table
-    require("plugins.configs.telescope")(plugin, opts)
-    require('telescope').setup({
+    local telescope = require("telescope")
+    local telescopeConfig = require("telescope.config")
+
+    -- Clone the default Telescope configuration
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+    -- I want to search in hidden/dot files.
+    table.insert(vimgrep_arguments, "--hidden")
+    -- I don't want to search in the `.git` directory.
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
+    telescope.setup({
       defaults = {
+        hidden = true,
         file_ignore_patterns = {
           "config/alfred/.*",
-          "./assets/fonts/.*"
-        }
-      }
+          "./assets/fonts/.*",
+        },
+      },
+      pickers = {
+		    find_files = {
+			    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*"},
+		    },
+	    },
     })
+
+    vim.api.nvim_create_user_command(
+      "Gco",
+      function(args)
+        local ticket_number = args.fargs[1]
+        require('telescope.builtin').git_branches({ pattern = 'refs/**/*'..ticket_number..'*'})
+      end,
+      {
+        nargs = "+",
+        complete = function()
+          return vim.tbl_keys(commands)
+        end,
+      }
+    )
 
     -- require telescope and load extensions as necessary
     -- local telescope = require "telescope"
