474d8ad4fac434d58c8c1945dc1d8da9d05b7d70
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-11-27 20:30:51 -0600

    adding symbolizer plugin and upating mapping

--- config/nvim/lua/user/lsp/mappings.lua
+++ config/nvim/lua/user/lsp/mappings.lua
@@ -2,11 +2,16 @@ return {
   n = {
     -- this mapping will only be set in buffers with an LSP attached
     K = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
-    -- ["gd"] = {
-    --   function ()
-    --     require('laravel-route-symbol')
-    --   end,
-    --   desc = "Go to definition",
-    -- },
+    ["gd"] = {
+      function ()
+        local symbolize = require('php-symbolize')
+        if symbolize.validate_touple() then
+          symbolize.go_to_definition()
+        else
+          require("telescope.builtin").lsp_definitions()
+        end
+      end,
+      desc = "Go to definition",
+    },
   },
 }
