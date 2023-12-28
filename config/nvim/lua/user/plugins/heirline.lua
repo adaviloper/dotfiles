701aa768e2577a1546bca1e21c89a02550295895
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-12-27 20:13:43 -0600

    updating winbar

--- config/nvim/lua/user/plugins/heirline.lua
+++ config/nvim/lua/user/plugins/heirline.lua
@@ -2,25 +2,6 @@ return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
     local status = require("astronvim.utils.status")
-    -- local grapple = require('grapple')
-    -- grapple.exists(vim.cmd("!echo '%:p'"))
-    opts.winbar = { -- winbar
-      init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-      fallthrough = false,
-      {
-        -- condition = function() return not status.condition.is_active() end,
-        status.component.separated_path(),
-        status.component.file_info({
-          file_icon = { hl = status.hl.file_icon("winbar"), padding = { left = 0 } },
-          file_modified = false,
-          file_read_only = false,
-          hl = status.hl.get_attributes("winbarnc", true),
-          surround = false,
-          update = "BufEnter",
-        }),
-      },
-      -- status.component.breadcrumbs({ hl = status.hl.get_attributes("winbar", true) }),
-    }
     opts.statusline = {
       -- statusline
       hl = { fg = "fg", bg = "bg" },
@@ -95,6 +76,23 @@ return {
       status.component.lsp(),
       status.component.treesitter(),
       status.component.nav(),
+    }
+    opts.winbar = { -- winbar
+      init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+      fallthrough = false,
+      {
+        -- condition = function() return not status.condition.is_active() end,
+        status.component.separated_path(),
+        status.component.file_info({
+          file_icon = { hl = status.hl.file_icon("winbar"), padding = { left = 0 } },
+          file_modified = false,
+          file_read_only = false,
+          hl = status.hl.get_attributes("winbarnc", true),
+          surround = false,
+          update = "BufEnter",
+        }),
+      },
+      status.component.breadcrumbs({ hl = status.hl.get_attributes("winbar", true) }),
     }
 
     return opts
