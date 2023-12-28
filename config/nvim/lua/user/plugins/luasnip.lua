30e427fd2677c1a1ff0f101ee8a0e20004def19c
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-12-27 20:15:06 -0600

    snippets still aren't working

--- config/nvim/lua/user/plugins/luasnip.lua
+++ config/nvim/lua/user/plugins/luasnip.lua
@@ -2,10 +2,25 @@ return {
   "L3MON4D3/LuaSnip",
   enabled = true,
   config = function(plugin, opts)
-    local ls = require("luasnip")
+    -- local ls = require("luasnip")
+    -- local types = require("luasnip.util.types")
     require "plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
-    ls.setup()
+    -- ls.setup({
+    --   history = true,
+    --
+    --   updateevents = 'TextChanged,TextChangedI',
+    --
+    --   enable_autosnippets = true,
+    --
+    --   ext_opts = {
+    --     [types.choiceNode] = {
+    --       active = {
+    --         virt_text = { { '<-', 'Error' } },
+    --       }
+    --     }
+    --   }
+    -- })
+
     -- require("luasnip.loaders.from_lua").load({ paths = { "./lua/user/snippets" } }) -- load snippets paths
-    return opts
   end,
 }
--- config/nvim/lua/user/snippets/php.lua
+++ config/nvim/lua/user/snippets/php.lua
@@ -1,21 +1,23 @@
 return {
-  s('pubf', 
-    {
-      fmt({
-        "public function (<>): <>",
-        "{",
-        "    <>",
-        "}",
-      },
-        {
-          i(1),
-          i(2),
-          i(3),
-        }
-      ),
-    }
-  )
-}, 
+  {
+    s('pubf',
+      {
+        fmt({
+            "public function (<>): <>",
+            "{",
+            "    <>",
+            "}",
+          },
+          {
+            i(1),
+            i(2),
+            i(3),
+          }
+        ),
+      }
+    )
+  },
   {
     s('autotest', t('this is an auto test'))
   }
+}
