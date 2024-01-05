b6f9ed996da5519de26a5c8766fb8449b3dc9b72
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-01-04 21:51:33 -0600

    adding tmuxp configurations

--- oh-my-zsh/custom/rcfiles/functions/tmux.zsh
+++ oh-my-zsh/custom/rcfiles/functions/tmux.zsh
@@ -3,7 +3,7 @@
 # Function to create or attach to a tmux session with specified windows and directories
 function tm() {
     SESSION_NAME="main"
-    SESSION_NAMES=("Dotfiles" "Loop Returns App" "QMK Firmware" "Whiplash Middleware")
+    PROJECTS=("dots" "loop" "qmk" "whiplash")
     DIRECTORIES=("~/.dotfiles" "~/Code/loop-returns-app" "~/Code/qmk_firmware" "~/Code/whiplash-middleware")
 
     # Function to check if a directory exists
@@ -22,27 +22,13 @@ function tm() {
         # Execute tmux commands passed as arguments
         tmux "$@"
     else
-        for ((i=1; i<=${#SESSION_NAMES[@]}; i++)); do
-            tmux has-session -t $SESSION_NAMES[$i] 2>/dev/null
-
-            if [ $? != 0 ]; then
-                # Check if the directory exists before setting it
-                if check_directory "${DIRECTORIES[$i]}"; then
-                    echo "Creating session for '${SESSION_NAMES[$i]}' in directory: ${DIRECTORIES[$i]//\~/$HOME} : ${DIRECTORIES[$i]}."
-                    tmux new-session -d -s $SESSION_NAMES[$i] -n "Project"
-
-                    # Add more windows with specific names and directories
-                    tmux new-window -t $SESSION_NAMES[$i]: -n "Terminals"
-                    tmux send-keys -t $SESSION_NAMES[$i]:1 "cd ${DIRECTORIES[$i]//\~/$HOME}" C-m
-                    tmux send-keys -t $SESSION_NAMES[$i]:2 "cd ${DIRECTORIES[$i]//\~/$HOME}" C-m
-                    tmux send-keys -t $SESSION_NAMES[$i]:1 "c" C-m
-                    tmux send-keys -t $SESSION_NAMES[$i]:2 "c" C-m
-                    tmux select-window -t $SESSION_NAMES[$i]:1
-                fi
+        for ((i=1; i<=${#DIRECTORIES[@]}; i++)); do
+            if check_directory "${DIRECTORIES[$i]}"; then
+                tmuxp load -d $PROJECTS[$i]
             else
                 echo "Directory '${DIRECTORIES[$i]//\~/$HOME}' does not exist."
             fi
         done
-        tmux attach -t $SESSION_NAMES[1]
+        tmux attach -t Dotfiles
     fi
 }
