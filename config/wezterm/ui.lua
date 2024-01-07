4d64bd3b6aa10cc0c0bb7aa99f4d7f13529316de
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-01-07 13:52:48 -0600

    stashing terminal transparency work

--- config/wezterm/ui.lua
+++ config/wezterm/ui.lua
@@ -4,6 +4,8 @@ function M.setup(config)
   config.color_scheme = 'Catppuccin Mocha'
   config.enable_tab_bar = false
   config.line_height = 1.0
+  -- config.macos_window_background_blur = 30
+  -- config.window_background_opacity = 0.85
   config.window_close_confirmation = 'AlwaysPrompt'
   config.window_decorations = 'RESIZE'
 end
