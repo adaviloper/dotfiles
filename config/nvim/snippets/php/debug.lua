fd536c70baaafdc4193534e2981512b656151c62
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-03-01 11:57:43 -0600

    adding ll snippet

--- config/nvim/snippets/php/debug.lua
+++ config/nvim/snippets/php/debug.lua
@@ -1,3 +1,15 @@
+local get_target_node = function(node_name)
+  local node = vim.treesitter.get_node()
+
+  while node ~= nil do
+    if node:type() == node_name then
+      return node
+    end
+
+    node = node:parent()
+  end
+end
+
 return
   -- Autosnippets
   {
@@ -12,23 +24,30 @@ dd({}__METHOD__ . ':' . __LINE__);
     s('ll',
       fmt(
         [[
-Log::{}({});
+Log::{}({}{});
     ]], {
-          c(1, {t('info'), t('debug')}),
-          i(2, ''),
+          i(1, 'info'),
+          f(function ()
+            local node = get_target_node('method_declaration')
+            if not node then
+              vim.notify('No target node found.')
+              return
+            end
+
+            local method_name = nil
+            local query = vim.treesitter.query.parse('php',  "[(method_declaration name: (_) @name)]")
+            for _, capture in query:iter_captures(node, 0) do
+              method_name = vim.treesitter.get_node_text(capture, 0)
+            end
+
+            if method_name ~= nil then
+              return "'".. vim.fn.expand('%:t:r') .."#" .. method_name .. "'"
+            end
+
+            return "'".. vim.fn.expand('%:t:r') .. "'"
+          end),
+          i(2, '')
         })),
-    -- Private function
---     s('prif',
---       fmt(
---         [[
--- private function {}()
--- {{
---     {}
--- }}
---     ]], {
---           i(1, 'method_name'),
---           i(2, ''),
---         })),
   },
   -- Autosnippets
   {
