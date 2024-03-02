1730c71cedb94b26ae3571cf1ad70d20c71c9780
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-03-01 22:37:26 -0600

    creating cwd_branch scope for sub-project navigation

--- config/nvim/lua/plugins/grapple.lua
+++ config/nvim/lua/plugins/grapple.lua
@@ -1,15 +1,33 @@
 return {
   'cbochs/grapple.nvim',
   config = function()
+    require('grapple').define_scope({
+		  name = "cwd_branch",
+		  desc = "Current working directory and git branch",
+		  fallback = "cwd",
+		  cache = {
+			  event = { "BufEnter", "FocusGained" },
+			  debounce = 1000, -- ms
+		  },
+		  resolver = function()
+			  local git_files = vim.fs.find(".git", { upward = true, stop = vim.loop.os_homedir() })
+			  if #git_files == 0 then
+				  return
+			  end
+
+			  local root = vim.loop.cwd()
+
+			  local result = vim.fn.system({ "git", "symbolic-ref", "--short", "HEAD" })
+			  local branch = vim.trim(string.gsub(result, "\n", ""))
+
+			  local id = string.format("%s:%s", root, branch)
+			  local path = root
+
+			  return id, path
+		  end,
+    })
     require('grapple').setup({
-      scope = 'git_branch',
-      -- scope = require('grapple.scope').suffix(
-      --   require('grapple').resolvers.directory,
-      --   require('grapple').resolvers.git_branch_suffix,
-      --   {
-      --     persist = true,
-      --   }
-      -- ),
+      scope = 'cwd_branch',
       popup_options = {
         width = 150,
       },
