fde1b00cc1880123a6b4498bde775290c863c5fd
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-03-23 20:29:59 -0500

    update to only load locally existing sessions

--- oh-my-zsh/custom/rcfiles/functions/tmux.zsh
+++ oh-my-zsh/custom/rcfiles/functions/tmux.zsh
@@ -5,7 +5,6 @@ function tm() {
 
     # Function to check if a directory exists
     check_directory() {
-        echo "$1"
         local expanded_path=$(eval echo $1)
         if [ -d "$expanded_path" ]; then
             return 0  # Directory exists
@@ -19,25 +18,33 @@ function tm() {
         # Execute tmux commands passed as arguments
         tmux "$@"
     else
-        SESSIONS="$(gum choose --no-limit "$CONFIG_DIR"/*.yaml)"
-        eval "CONFIG_FILES=($SESSIONS)"
-        for config_path in $CONFIG_FILES; do
-            # Check if the tmuxp config file exists
-            if [ -f "$config_path" ]; then
-                # Parse session_name from the YAML file
-                local session_name=$(yq e '.session_name' "$config_path")
-                # Parse the first start_directory from the YAML file
-                local start_directory=$(yq e '.start_directory' "$config_path" | awk 'NR==1')
-
-                if [ -z "$start_directory" ] || check_directory "$start_directory"; then
-                    # Load the tmuxp session using the config file
-                    tmuxp load -d "$config_path"
-                else
-                    echo "Start directory '$start_directory' for config '$config_path' does not exist."
-                fi
+        # Get a list of YAML files in the CONFIG_DIR
+        YAML_FILES=("$CONFIG_DIR"/*.yaml)
+        
+        # Filter YAML files based on start_directory existence
+        FILTERED_YAML=()
+        for yaml_file in "${YAML_FILES[@]}"; do
+            start_directory=$(yq e '.start_directory' "$yaml_file" | awk 'NR==1')
+            if [ -z "$start_directory" ] || check_directory "$start_directory"; then
+                FILTERED_YAML+=("$yaml_file")
             fi
         done
-        tmux attach -t Dotfiles
+
+        # If there are valid YAML files, choose one using gum choose
+        if [ ${#FILTERED_YAML[@]} -gt 0 ]; then
+            SESSIONS="$(gum choose --header "Choose which sessions to load" --no-limit "${FILTERED_YAML[@]}")"
+            eval "CONFIG_FILES=($SESSIONS)"
+            for config_path in $CONFIG_FILES; do
+                # Check if the tmuxp config file exists
+                if [ -f "$config_path" ]; then
+                    # Load the tmuxp session using the config file
+                    tmuxp load -d "$config_path"
+                fi
+            done
+            tmux attach -t Dotfiles
+        else
+            echo "No valid YAML files found in '$CONFIG_DIR' or start directories do not exist."
+        fi
     fi
 }
 
