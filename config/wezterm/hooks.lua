0049a41658218cbede0a307b9bc3428a7044a0c8
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-03-25 22:06:36 -0500

    adjusting on-gui-start event actions

--- config/wezterm/hooks.lua
+++ config/wezterm/hooks.lua
@@ -12,12 +12,12 @@ function M.setup(_)
       local padding = 30
       local menu_bar_height = 25
 
-      if active_screen.width > 2560 then
+      if active_screen.width > 3750 then
         window:gui_window():set_position((active_screen.width) / 4 + padding, padding + menu_bar_height)
         window:gui_window():set_inner_size((active_screen.width - (padding * 4)) / 2, active_screen.height - (padding * 2) - menu_bar_height)
       else
-        window:gui_window():set_position(padding, padding + menu_bar_height)
-        window:gui_window():set_inner_size((active_screen.width - padding * 2), active_screen.height - (padding * 2 - menu_bar_height))
+        window:gui_window():set_position(padding * 2, padding * 2 + menu_bar_height * 2)
+        window:gui_window():set_inner_size((active_screen.width - padding * 4), active_screen.height - (padding * 4) - menu_bar_height * 2)
       end
     end
   )
