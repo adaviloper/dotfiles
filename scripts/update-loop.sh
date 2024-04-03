f84723b79530f73c33251626ca80856131aa5288
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-04-03 09:41:54 -0500

    updating to verify we should switch branches first

--- scripts/update-loop.sh
+++ scripts/update-loop.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Define the repository path
+repo_path="$HOME/Code/loop-returns-app"
+
+# Check if the repository path exists
+if [ ! -d "$repo_path" ]; then
+    echo "Error: Repository path $repo_path does not exist."
+    exit 1
+fi
+
 echo "Switching to loop-returns-app"
-cd ~/Code/loop-returns-app/
-echo "Checking out [develop]"
+cd "$repo_path" || exit 1
 
 # Track whether changes were stashed during script execution
 changes_stashed=false
@@ -14,20 +22,28 @@ if ! git diff-index --quiet HEAD --; then
     changes_stashed=true
 fi
 
-echo "Checking out [develop]"
-git checkout develop || exit 1
-git pull || exit 1
+# Check if the current branch is not develop
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "develop" ]; then
+    echo "Checking out [develop]"
+    git checkout develop || exit 1
+    git pull || exit 1
 
-echo "Running update script"
-if [ -f "script/update" ]; then
-    sh "script/update" || exit 1
-else
-    echo "Error: update script not found."
-    exit 1
+    # Run the update script only if the branch was switched
+    echo "Running update script"
+    if [ -f "script/update" ]; then
+        sh "script/update" || exit 1
+    else
+        echo "Error: update script not found."
+        exit 1
+    fi
 fi
 
-echo "Returning back to previous branch"
-git checkout - || exit 1
+# Return back to the previous branch if it was not develop initially
+if [ "$current_branch" != "develop" ]; then
+    echo "Returning back to previous branch"
+    git checkout - || exit 1
+fi
 
 # Pop stashed changes if they were stashed during script execution
 if $changes_stashed; then
