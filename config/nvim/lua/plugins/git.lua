8f7b06ffd895c96553778459d38aa6ca736a69f4
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-04-16 08:23:24 -0500

    formatting

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -4,7 +4,7 @@ return {
   },
   {
     "lewis6991/gitsigns.nvim",
-    enabled = vim.fn.executable "git" == 1,
+    enabled = vim.fn.executable("git") == 1,
     event = "User AstroGitFile",
     opts = {
       current_line_blame = true,
