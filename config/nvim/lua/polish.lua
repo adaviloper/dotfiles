23bddc0ef85dd45008ec7d585cf14fbc1b9f7068
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-04-26 16:53:28 -0500

    adding global print function

--- config/nvim/lua/polish.lua
+++ config/nvim/lua/polish.lua
@@ -1,6 +1,10 @@
 -- This file is automatically ran last in the setup process and is a good place to configure
 -- augroups/autocommands and custom filetypes also this just pure lua so
 -- anything that doesn't fit in the normal config locations above can go here
+function P(item)
+  vim.notify(vim.inspect(item))
+end
+
 
 -- Set up custom filetypes
 vim.filetype.add {
