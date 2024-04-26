5690bf3fe40702480cf357eb75e1fa9fb4ce3294
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-04-26 11:50:09 -0500

    updating dml snippet to automatically include var

--- config/nvim/snippets/php/debug.lua
+++ config/nvim/snippets/php/debug.lua
@@ -11,7 +11,7 @@ local get_target_node = function(node_name)
 end
 
 return
-  -- Snippets
+  -- Autosnippets
   {
     -- Public function 
     s('dml',
@@ -19,14 +19,38 @@ return
         [[
 dd({}__METHOD__ . ':' . __LINE__);
     ]], {
-          i(1, ''),
+          sn(
+            1,
+            {
+              d(1, function (args)
+                local pos = vim.api.nvim_win_get_cursor(0)
+                local prev_line = vim.api.nvim_buf_get_lines(0, pos[1] - 2, pos[1] - 1, false)
+                local statement = vim.split(prev_line[1], '=', { trimempty = true })
+
+                if string.find(prev_line[1], '=') ~= nil then
+                  return sn(0,
+                    {
+                      t(vim.trim(statement[1]) .. ', '),
+                      i(0, ''),
+                    }
+                  )
+                end
+
+                if statement[1] == nil or statement[1] == '' then
+                  return sn(nil, {
+                    i(1, '')
+                  })
+                end
+              end)
+            }
+          ),
         })),
     s('ll',
       fmt(
         [[
 Log::{}({}{});
     ]], {
-          c(1, { t('info'), t('error'), t('warning') }),
+          c(1, { t('info'), t('error'), t('warn') }),
           f(function ()
             local node = get_target_node('method_declaration')
             if not node then
