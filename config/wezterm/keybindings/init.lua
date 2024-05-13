a73b00315e9333b7ca30856867118a51003c66c5
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-05-13 13:46:26 -0500

    stripping down wezterm defaults

--- config/wezterm/keybindings/init.lua
+++ config/wezterm/keybindings/init.lua
@@ -1,18 +1,27 @@
+local defaults = require('keybindings.defaults')
 local tmux = require('keybindings.tmux')
 
 local M = {}
 
 function M.setup(config)
+  config.disable_default_key_bindings = true
   config.keys = {}
 
   local groups = {
-    tmux
+    defaults,
+    tmux,
   }
 
   for _, group in ipairs(groups) do
-    local bindings = group.getKeybindings()
-    for _, binding in ipairs(bindings) do
+    local keyBindings = group.getKeybindings()
+    for _, binding in ipairs(keyBindings) do
       table.insert(config.keys, binding)
+    end
+    if group.getKeyTableBindings then
+      local keytableBindings = group.getKeyTableBindings()
+      for _, binding in ipairs(keytableBindings) do
+        table.insert(config.keys_tables, binding)
+      end
     end
   end
 end
