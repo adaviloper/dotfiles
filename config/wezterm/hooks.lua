faecb01d091326b9e77f9d8aa5227d048d6f05c8
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-05-17 19:10:44 -0600

    updating laptop starting position and dimensions

--- config/wezterm/hooks.lua
+++ config/wezterm/hooks.lua
@@ -12,15 +12,14 @@ function M.setup(_)
       local padding = 30
       local menu_bar_height = 25
       local monitor_ratio = 31 / 9
-      local laptop_ratio = 1792 / 1120
       local current_ratio = active_screen.width / active_screen.height
 
       if current_ratio >= monitor_ratio then
         window:gui_window():set_position((active_screen.width) / 4 + padding, padding + menu_bar_height)
         window:gui_window():set_inner_size((active_screen.width - (padding * 4)) / 2, active_screen.height - (padding * 2) - menu_bar_height)
-      elseif current_ratio >= laptop_ratio then
+      else
         window:gui_window():set_position(padding * 2, padding * 2 + menu_bar_height * 2)
-        window:gui_window():set_inner_size((active_screen.width - padding * 4), active_screen.height - (padding * 4) - menu_bar_height * 2)
+        window:gui_window():set_inner_size((active_screen.width - padding * 4), active_screen.height - (padding * 2) - menu_bar_height)
       end
     end
   )
