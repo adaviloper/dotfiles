eb0d62f5f25c128a8fd25d9e18ace08cf02a2e36
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-05-19 16:26:23 -0500

    adding vclear alias

--- oh-my-zsh/custom/rcfiles/aliases/terminal.zsh
+++ oh-my-zsh/custom/rcfiles/aliases/terminal.zsh
@@ -1,5 +1,6 @@
 alias c="clear"
 alias cdh="cd ~/Homestead"
 alias cloc="git ls-files | xargs wc -l"
+alias vclear="rm -rf ~/.local/state/nvim; rm -rf ~/.local/share/nvim; rm -rf ~/.cache/nvim/"
 alias -g G="| rg"
 
