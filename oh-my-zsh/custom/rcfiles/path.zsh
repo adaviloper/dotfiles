2959500e6d46b5630a1019874c6d1ff424a24c27
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-05-19 19:52:18 -0500

    updating path and color settings in tmux

--- oh-my-zsh/custom/rcfiles/path.zsh
+++ oh-my-zsh/custom/rcfiles/path.zsh
@@ -1,12 +1,2 @@
-export PATH="/Applications/PhpStorm.app/Contents/bin
-:/opt/homebrew/opt/avr-gcc@8/bin
-:$HOME/.config/bin/git
-:$HOME/.config/bin
-:$HOME/go/bin
-:/usr/local/opt/arm-none-eabi-gcc@8/bin
-:/usr/local/opt/arm-gcc-bin@8/bin
-:$HOME/.local/share/nvim/mason/bin
-:Appyications/PhpStorm.app/Contents/bin
-:/Applications/XAMPP/bin
-:yarn global bin:$PATH"
+export PATH="/Applications/PhpStorm.app/Contents/bin:/opt/homebrew/opt/avr-gcc@8/bin:$HOME/.config/bin/git:$HOME/.config/bin:$HOME/go/bin:/usr/local/opt/arm-none-eabi-gcc@8/bin:/usr/local/opt/arm-gcc-bin@8/bin:$HOME/.local/share/nvim/mason/bin:Appyications/PhpStorm.app/Contents/bin:/Applications/XAMPP/bin:yarn global bin:$PATH"
 
