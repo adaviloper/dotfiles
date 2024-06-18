f8eb92b0df2cdb6e6d4ef0a0d57325b0b3e2445b
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-06-18 13:43:00 -0500

    moving docker aliases to aliases directory

--- oh-my-zsh/custom/rcfiles/functions/docker.zsh
+++ oh-my-zsh/custom/rcfiles/functions/docker.zsh
@@ -1,9 +1,3 @@
-alias dd="docker-compose down"
-alias dup="docker-compose up -d"
-alias dwhip="docker exec -it whiplash-app"
-alias dwhipart="docker exec -it whiplash-app php artisan"
-alias dwhiptest="docker exec -it whiplash-app ./vendor/bin/phpunit"
-
 # function dcore() {
 #   if [[ "$1" -e "artisan"]] then
 #     docker exec -it core php artisan
