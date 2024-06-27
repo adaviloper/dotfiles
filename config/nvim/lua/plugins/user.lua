2783fb61a1aaaa94dab6c59a86aa2ccf92d1b0e5
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-06-27 13:28:02 -0500

    formatting

--- config/nvim/lua/plugins/user.lua
+++ config/nvim/lua/plugins/user.lua
@@ -1,28 +1,28 @@
-local mocha = require("catppuccin.palettes").get_palette("mocha")
+local mocha = require('catppuccin.palettes').get_palette('mocha')
 
 -- You can also add new plugins here as well using the lazy syntax:
 return {
-  "andweeb/presence.nvim",
+  'andweeb/presence.nvim',
   {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
+    'ray-x/lsp_signature.nvim',
+    event = 'BufRead',
     config = function()
-      require("lsp_signature").setup()
+      require('lsp_signature').setup()
     end,
   },
 
   -- customize alpha options
   {
-    "goolord/alpha-nvim",
+    'goolord/alpha-nvim',
     opts = function(_, opts)
       -- customize the dashboard header
 
-      vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = mocha.blue })
-      vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = mocha.green, bg = mocha.blue })
-      vim.api.nvim_set_hl(0, "NeovimDashboardLogo2_1", { fg = mocha.blue, bg = mocha.green })
-      vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = mocha.green })
-      vim.api.nvim_set_hl(0, "NeovimDashboardFooter1", { fg = mocha.blue })
-      vim.api.nvim_set_hl(0, "NeovimDashboardFooter2", { fg = mocha.green })
+      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo1', { fg = mocha.blue })
+      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo2', { fg = mocha.green, bg = mocha.blue })
+      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo2_1', { fg = mocha.blue, bg = mocha.green })
+      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo3', { fg = mocha.green })
+      vim.api.nvim_set_hl(0, 'NeovimDashboardFooter1', { fg = mocha.blue })
+      vim.api.nvim_set_hl(0, 'NeovimDashboardFooter2', { fg = mocha.green })
       opts.section.header.val = {
         [[     ██        ]],
         [[    ███        ]],
@@ -37,23 +37,23 @@ return {
         [[N  E  O  V  I  M]],
       }
       opts.section.header.opts.hl = {
-        { { "NeovimDashboardLogo1", 2, 45 } },
-        { { "NeovimDashboardLogo1", 2, 45 } },
-        { { "NeovimDashboardLogo1", 2, 45 } },
-        { { "NeovimDashboardLogo3", 2, 14 }, { "NeovimDashboardLogo1", 15, 20 }, { "NeovimDashboardLogo3", 20, 45 } },
-        { { "NeovimDashboardLogo1", 2, 17 }, { "NeovimDashboardLogo3", 18, 41 } },
-        { { "NeovimDashboardLogo1", 2, 19 }, { "NeovimDashboardLogo3", 16, 45 } },
-        { { "NeovimDashboardLogo1", 2, 19 }, { "NeovimDashboardLogo3", 15, 45 } },
-        { { "NeovimDashboardLogo3", 0, 45 } },
-        { { "NeovimDashboardLogo1", 0, 45 } },
+        { { 'NeovimDashboardLogo1', 2, 45 } },
+        { { 'NeovimDashboardLogo1', 2, 45 } },
+        { { 'NeovimDashboardLogo1', 2, 45 } },
+        { { 'NeovimDashboardLogo3', 2, 14 }, { 'NeovimDashboardLogo1', 14, 20 }, { 'NeovimDashboardLogo3', 20, 45 } },
+        { { 'NeovimDashboardLogo1', 2, 17 }, { 'NeovimDashboardLogo3', 18, 41 } },
+        { { 'NeovimDashboardLogo1', 2, 19 }, { 'NeovimDashboardLogo3', 16, 45 } },
+        { { 'NeovimDashboardLogo1', 2, 19 }, { 'NeovimDashboardLogo3', 15, 45 } },
+        { { 'NeovimDashboardLogo3', 0, 45 } },
+        { { 'NeovimDashboardLogo1', 0, 45 } },
         {},
-        { { "NeovimDashboardFooter1", 0, 8 }, { "NeovimDashboardFooter2", 9, 22 } },
+        { { 'NeovimDashboardFooter1', 0, 8 }, { 'NeovimDashboardFooter2', 9, 22 } },
       }
       return opts
     end,
   },
   -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
+  { 'max397574/better-escape.nvim', enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
