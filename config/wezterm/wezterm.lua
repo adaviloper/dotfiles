e34f2228679f32d3c053eeddc610cb7a6052a7b9
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-06-26 19:12:49 -0500

    formatting

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,11 +1,11 @@
-local keybindings = require('keybindings')
-local font = require('font')
-local ui = require('ui')
+local keybindings = require("keybindings")
+local font = require("font")
+local ui = require("ui")
 local hooks = require("hooks")
 local config = {}
 
-for _, value in pairs({keybindings, font, ui, hooks}) do
-  value.setup(config)
+for _, value in pairs({ keybindings, font, ui, hooks }) do
+	value.setup(config)
 end
 
 config.term = "wezterm"
