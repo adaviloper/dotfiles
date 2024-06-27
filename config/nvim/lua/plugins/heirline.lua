b46f8006f34b95e44df48661160f7420e57552c6
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-06-26 19:09:48 -0500

    formatting

--- config/nvim/lua/plugins/heirline.lua
+++ config/nvim/lua/plugins/heirline.lua
@@ -1,24 +1,24 @@
-local mocha = require("catppuccin.palettes").get_palette("mocha")
+local mocha = require('catppuccin.palettes').get_palette('mocha')
 
 return {
-  "rebelot/heirline.nvim",
+  'rebelot/heirline.nvim',
   opts = function(_, opts)
-    local status = require("astroui.status")
+    local status = require('astroui.status')
     local grapple = require('grapple')
     local conditions = require('heirline.conditions')
 
     opts.statusline = { -- statusline
-      hl = { fg = "fg", bg = "bg" },
+      hl = { fg = 'fg', bg = 'bg' },
       status.component.mode({
         -- enable mode text with padding as well as an icon before it
         mode_text = {
-          icon = { kind = "VimIcon", padding = { right = 1, left = 0 } },
+          icon = { kind = 'VimIcon', padding = { right = 1, left = 0 } },
           padding = { right = 0, left = 0 },
         },
         -- surround the component with a separators
         surround = {
           -- it's a left element, so use the left separator
-          separator = "left",
+          separator = 'left',
           -- set the color of the surrounding based on the current mode using astronvim.utils.status module
           color = function()
             return { main = status.hl.mode_bg() }
@@ -27,11 +27,13 @@ return {
       }),
       status.component.git_branch(),
       status.component.file_info({
-        provider = function (self)
+        provider = function(self)
           -- first, trim the pattern relative to the current directory. For other
           -- options, see :h filename-modifers
-          local filename = vim.fn.fnamemodify(self.filename, ":.")
-          if filename == "" then return "[No Name]" end
+          local filename = vim.fn.fnamemodify(self.filename, ':.')
+          if filename == '' then
+            return '[No Name]'
+          end
           -- now, if the filename would occupy more than 1/4th of the available
           -- space, we trim the file path to its initials
           -- See Flexible Components section below for dynamic truncation
@@ -41,14 +43,14 @@ return {
           if grapple.exists() and grapple.name_or_index() ~= nil then
             return '[' .. grapple.name_or_index() .. ']'
           end
-        end
+        end,
       }),
       status.component.git_diff(),
       status.component.diagnostics(),
       status.component.fill(),
       status.component.cmd_info({
         macro_recording = {
-          icon = { kind = "MacroRecording", padding = { right = 1 } },
+          icon = { kind = 'MacroRecording', padding = { right = 1 } },
           hl = { fg = mocha.red },
         },
       }),
@@ -57,7 +59,7 @@ return {
       status.component.virtual_env(),
       status.component.treesitter(),
       status.component.nav(),
-      status.component.mode({ surround = { separator = "blank" } }),
+      status.component.mode({ surround = { separator = 'blank' } }),
     }
 
     opts.winbar = nil
@@ -67,19 +69,18 @@ return {
         condition = function(self)
           self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
           return status.condition.buffer_matches(
-            { filetype = { "aerial", "dapui_.", "neo%-tree", "NvimTree" } },
+            { filetype = { 'aerial', 'dapui_.', 'neo%-tree', 'NvimTree' } },
             vim.api.nvim_win_get_buf(self.winid)
           )
         end,
         provider = function(self)
-          return string.rep(" ", vim.api.nvim_win_get_width(self.winid) + 1)
+          return string.rep(' ', vim.api.nvim_win_get_width(self.winid) + 1)
         end,
-        hl = { bg = "tabline_bg" },
+        hl = { bg = 'tabline_bg' },
       },
 
-
       status.component.file_info({
-        hl = { fg = mocha.maroon },
+        hl = { fg = mocha.green },
         filename = {
           modify = ':p:.',
         },
@@ -91,7 +92,7 @@ return {
           color = mocha.surface0,
         },
       }),
-      status.component.fill({ hl = { bg = "tabline_bg" } }), -- fill the rest of the tabline with background color
+      status.component.fill({ hl = { bg = 'tabline_bg' } }), -- fill the rest of the tabline with background color
       { -- tab list
         condition = function()
           return #vim.api.nvim_list_tabpages() >= 2
@@ -99,23 +100,20 @@ return {
         status.heirline.make_tablist({ -- component for each tab
           provider = status.provider.tabnr(),
           hl = function(self)
-            return status.hl.get_attributes(
-              status.heirline.tab_type(self, "tab"),
-              true
-            )
+            return status.hl.get_attributes(status.heirline.tab_type(self, 'tab'), true)
           end,
         }),
         { -- close button for current tab
           provider = status.provider.close_button({
-            kind = "TabClose",
+            kind = 'TabClose',
             padding = { left = 1, right = 1 },
           }),
-          hl = status.hl.get_attributes("tab_close", true),
+          hl = status.hl.get_attributes('tab_close', true),
           on_click = {
             callback = function()
-              require("astroui.buffer").close_tab()
+              require('astroui.buffer').close_tab()
             end,
-            name = "heirline_tabline_close_tab_callback",
+            name = 'heirline_tabline_close_tab_callback',
           },
         },
       },
