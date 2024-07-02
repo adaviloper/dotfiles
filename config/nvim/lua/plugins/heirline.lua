31a2b25d7171b6c7bf7109c0d8605543084cceb0
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-07-02 11:34:06 -0500

    adding tab index to winbar

--- config/nvim/lua/plugins/heirline.lua
+++ config/nvim/lua/plugins/heirline.lua
@@ -89,6 +89,31 @@ return {
 						color = mocha.surface0,
 					},
 				}),
+				status.component.fill(),
+				{ -- tab list
+					condition = function()
+						return #vim.api.nvim_list_tabpages() >= 2
+					end, -- only show tabs if there are more than one
+					status.heirline.make_tablist({ -- component for each tab
+						provider = status.provider.tabnr(),
+						hl = function(self)
+							return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true)
+						end,
+					}),
+					{ -- close button for current tab
+						provider = status.provider.close_button({
+							kind = "TabClose",
+							padding = { left = 1, right = 1 },
+						}),
+						hl = status.hl.get_attributes("tab_close", true),
+						on_click = {
+							callback = function()
+								require("astroui.buffer").close_tab()
+							end,
+							name = "heirline_tabline_close_tab_callback",
+						},
+					},
+				},
       },
       -- active winbar
       {
@@ -117,6 +142,31 @@ return {
 						color = mocha.surface0,
 					},
 				}),
+				status.component.fill(),
+				{ -- tab list
+					condition = function()
+						return #vim.api.nvim_list_tabpages() >= 2
+					end, -- only show tabs if there are more than one
+					status.heirline.make_tablist({ -- component for each tab
+						provider = status.provider.tabnr(),
+						hl = function(self)
+							return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true)
+						end,
+					}),
+					{ -- close button for current tab
+						provider = status.provider.close_button({
+							kind = "TabClose",
+							padding = { left = 1, right = 1 },
+						}),
+						hl = status.hl.get_attributes("tab_close", true),
+						on_click = {
+							callback = function()
+								require("astroui.buffer").close_tab()
+							end,
+							name = "heirline_tabline_close_tab_callback",
+						},
+					},
+				},
       },
     }
 
