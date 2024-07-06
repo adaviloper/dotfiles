c9312322fe544f5d172e2fe4aa4088b91eea5dae
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-07-06 12:10:44 -0500

    updating to handle multiple target types

--- config/nvim/lua/helpers/treesitter.lua
+++ config/nvim/lua/helpers/treesitter.lua
@@ -3,11 +3,14 @@ local parsers = require "nvim-treesitter.parsers"
 
 local M = {}
 
-M.get_target_node = function(node_name)
+M.get_target_node = function(node_names)
+  if type(node_names) ~= 'table' then
+    node_names = { node_names }
+  end
   local node = vim.treesitter.get_node()
 
   while node ~= nil do
-    if node:type() == node_name then
+    if vim.tbl_contains(node_names, node:type()) then
       return node
     end
 
@@ -19,12 +22,16 @@ end
 
 M.parse_query_for_capture = function (node, query_to_parse, target)
   local query = ts.query.parse(parsers.get_buf_lang(), query_to_parse)
+  local captures = {}
   for i, match, _ in query:iter_captures(node, 0) do
     local name = query.captures[i]
+    vim.notify(name)
     if name == target then
-      return ts.get_node_text(match, 0)
+      table.insert(captures, ts.get_node_text(match, 0))
     end
   end
+
+  return captures
 
 end
 
