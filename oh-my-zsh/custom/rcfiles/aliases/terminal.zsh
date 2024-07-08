7e3fd9889da14a0ce1583d8d474f5bb24c2187ce
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-07-08 16:39:34 -0500

    adding sh file for tail-ing log files

--- oh-my-zsh/custom/rcfiles/aliases/terminal.zsh
+++ oh-my-zsh/custom/rcfiles/aliases/terminal.zsh
@@ -2,5 +2,8 @@ alias c="clear"
 alias cdh="cd ~/Homestead"
 alias cloc="git ls-files | xargs wc -l"
 alias vclear="rm -rf ~/.local/state/nvim; rm -rf ~/.local/share/nvim; rm -rf ~/.cache/nvim/"
+
 alias -g G="| rg"
+alias -g J="| jq"
+alias -g Y="| yq"
 
