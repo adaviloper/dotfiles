4269b956559b70bebc032861c94e7d6194d6ee10
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-07-14 19:20:00 -0500

    adding vue community pack

--- config/nvim/lua/community.lua
+++ config/nvim/lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.vue" },
   -- import/override with your plugins folder
 }
