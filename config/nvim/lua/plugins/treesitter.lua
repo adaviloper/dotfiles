1f325d67365da04455fb6197aeba8573828f1ad0
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-07-14 19:21:05 -0500

    updating treesitter configs

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,10 +4,41 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
+    auto_install = true,
     ensure_installed = {
+      "javascript",
       "lua",
+      "typescript",
       "vim",
+      "vue",
+      "yaml",
       -- add more arguments for adding more treesitter parsers
+    },
+    highlight = {
+      additional_vim_regex_highlighting = true,
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<leader>ss",
+        node_incremental = "<leader>si",
+        scope_incremental = "<leader>sc",
+        node_decremental = "<leader>sd",
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+        },
+      },
     },
   },
 }
