4db80232d14c28b05575e879841decb09358e384
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-07-26 20:07:36 -0500

    updating highlihgts to neovim api syntax

--- config/nvim/lua/config/highlights.lua
+++ config/nvim/lua/config/highlights.lua
@@ -1,5 +1,18 @@
-vim.cmd([[highlight DiagnosticUnderlineError cterm=undercurl gui=undercurl guisp=#f38ba8]])
-vim.cmd([[highlight DiagnosticUnderlineWarn cterm=undercurl gui=undercurl guisp=#f9e2af]])
-vim.cmd([[highlight DiagnosticUnderlineInfo cterm=undercurl gui=undercurl guisp=#89dceb]])
-vim.cmd([[highlight SpellBad cterm=undercurl gui=undercurl guisp=#a6e3a1]])
-
+-- Highlight the @foo.bar capture group with the "Identifier" highlight group
+vim.api.nvim_set_hl(0, "@lsp.type.property", { fg = "#89dceb" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
+  undercurl = true,
+  sp = "#f38ba8"
+})
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", {
+  undercurl = true,
+  sp = "#f9e2af"
+})
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", {
+  undercurl = true,
+  sp = "#89dceb"
+})
+vim.api.nvim_set_hl(0, "SpellBad", {
+  undercurl = true,
+  sp = "#a6e3a1"
+})
