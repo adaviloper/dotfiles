208b4e391b4062fbe38e405d8bed1770257668ca
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-07-27 21:47:40 -0500

    removing property color

--- config/nvim/lua/config/highlights.lua
+++ config/nvim/lua/config/highlights.lua
@@ -1,5 +1,4 @@
 -- Highlight the @foo.bar capture group with the "Identifier" highlight group
-vim.api.nvim_set_hl(0, "@lsp.type.property", { fg = "#89dceb" })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
   undercurl = true,
   sp = "#f38ba8"
