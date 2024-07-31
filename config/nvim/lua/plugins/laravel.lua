4b7513a515b75b80c68e536ad3dd17b0a790127a
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-07-31 18:01:46 -0500

    commenting out Laravel plugin commands

--- config/nvim/lua/plugins/laravel.lua
+++ config/nvim/lua/plugins/laravel.lua
@@ -7,11 +7,11 @@ return {
     "nvimtools/none-ls.nvim",
   },
   cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-  keys = {
-    { "<leader>la", ":Laravel artisan<cr>" },
-    { "<leader>lr", ":Laravel routes<cr>" },
-    { "<leader>lm", ":Laravel related<cr>" },
-  },
+  -- keys = {
+  --   { "<leader>la", ":Laravel artisan<cr>" },
+  --   { "<leader>lr", ":Laravel routes<cr>" },
+  --   { "<leader>lm", ":Laravel related<cr>" },
+  -- },
   event = { "VeryLazy" },
   opts = function (_, opts)
     opts.features = {
