a1a8f3f2383bf65a2229583a8be76faa2ecb0d22
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-08-05 12:54:48 -0500

    adding MarkView plugin

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,34 +1,25 @@
 return {
   {
-    'MeanderingProgrammer/markdown.nvim',
-    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    "OXY2DEV/markview.nvim",
+    lazy = false,      -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'echasnovski/mini.nvim',
-    }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    config = function()
-      require('render-markdown').setup({
-        bullet = {
-          -- Turn on / off list bullet rendering
-          enabled = true,
-          -- Replaces '-'|'+'|'*' of 'list_item'
-          -- How deeply nested the list is determines the 'level'
-          -- The 'level' is used to index into the array using a cycle
-          -- If the item is a 'checkbox' a conceal is used to hide the bullet instead
-          icons = { '●', '○', '◆', '◇' },
-          -- Highlight for the bullet icon
-          highlight = 'Normal',
-        },
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function ()
+      local markview = require('markview')
+      local presets = require('markview.presets')
+
+      markview.setup({
+        heading = presets.headings.glow_labels,
+        hybrid_modes = { 'i', 'n' }
       })
     end,
-  },
-  {
-    "tadmccorkle/markdown.nvim",
-    ft = "markdown", -- or 'event = "VeryLazy"'
-    opts = {
-      -- configuration here or empty for defaults
-    },
   }
 }
