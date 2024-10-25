6bd93d77673771e6af63430d9c1e6fc01f8496ca
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-10-24 21:06:05 -0500

    nvim updates

--- config/nvim/snippets/typescript/debug.lua
+++ config/nvim/snippets/typescript/debug.lua
@@ -3,12 +3,20 @@ return
     s(
       'cml',
       fmt(
-        [[console.{}('typescript', {});]],
+        [[console.{}('{}'{});]],
         {
           c(1, { t('log'), t('error'), t('table') }),
-          i(2, ''),
+          f(
+            function ()
+              local line_number = vim.api.nvim_win_get_cursor(0)
+              return vim.fn.expand('%:t') .. ':' .. line_number[1]
+            end
+          ),
+          i(2, '')
         }
       )
     )
   },
   {}
+
+
