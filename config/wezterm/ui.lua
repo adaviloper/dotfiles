add4e10f72afb3cbf4818a7f1481dadfe4705fcb
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-11-09 12:21:28 -0600

    maintain window size as font size changes

--- config/wezterm/ui.lua
+++ config/wezterm/ui.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup(config)
+  config.adjust_window_size_when_changing_font_size = false
   config.color_scheme = 'Catppuccin Mocha'
   config.enable_tab_bar = false
   config.line_height = 1.0
