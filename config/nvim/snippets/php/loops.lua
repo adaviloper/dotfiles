e07f3f06e5bc4a17b845cb96202610f2e4081a60
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-02-11 19:47:01 -0600

    adding auto-param naming

--- config/nvim/snippets/php/loops.lua
+++ config/nvim/snippets/php/loops.lua
@@ -1,14 +1,33 @@
+local camel_case = function (args)
+  local letters = vim.split(args[1][1], '')
+  if #letters == 0 then
+    return ''
+  end
+  if string.find(args[1][1], 'Request') then
+    return '$request'
+  end
+  letters[1] = string.lower(letters[1])
+  return '$' .. table.concat(letters, '')
+end
+
 local collection_pipeline = function (parent, method)
   return fmt(
     [[
-      {}->]] .. method .. [[(function (${}) {{
-          return ${};
+      {}->]] .. method .. [[(function ({} {}) {{
+          return {};
       }})
       ]],
     {
       t(parent.env.POSTFIX_MATCH),
-      t(parent.env.POSTFIX_MATCH),
-      t(parent.env.POSTFIX_MATCH),
+      i(1, ''),
+      f(
+        camel_case,
+        {1}
+      ),
+      f(
+        camel_case,
+        {1}
+      ),
     }
   )
 end
