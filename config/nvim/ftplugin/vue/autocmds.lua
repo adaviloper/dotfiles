59fc4f25d5b2925b446b4377879b64614ace3fdb
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-02-11 18:10:42 -0600

    adding return value for whether text was inserted

--- config/nvim/ftplugin/vue/autocmds.lua
+++ config/nvim/ftplugin/vue/autocmds.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   pattern = '*.vue',
   group = ts_group,
   callback = function (params)
-    require('config.commands.tpl').insert_template({
+    local template_inserted = require('config.commands.tpl').insert_template({
       '<template>',
       '  <div>',
       '  </div>',
@@ -18,6 +18,8 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
       '',
       '</style>',
     })
-    vim.api.nvim_win_set_cursor(0, { 7, 1 })
+    if template_inserted then
+      vim.api.nvim_win_set_cursor(0, { 7, 1 })
+    end
   end,
 })
--- config/nvim/lua/config/commands/tpl.lua
+++ config/nvim/lua/config/commands/tpl.lua
@@ -4,7 +4,10 @@ M.insert_template = function(template)
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
   if #lines == 1 and lines[1] == '' then
     vim.api.nvim_buf_set_lines(0, 0, -1, false, template)
+    return true
   end
+
+  return false
 end
 
 return M
