784f5088ea22167886cf9cb16d54ef0ab78f8acd
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-02-11 18:10:54 -0600

    consolidating laravel plugins

--- config/nvim/lua/plugins/laravel.lua
+++ config/nvim/lua/plugins/laravel.lua
@@ -1,69 +1,74 @@
 return {
-  "adalessa/laravel.nvim",
-  dependencies = {
-    "tpope/vim-dotenv",
-    "nvim-telescope/telescope.nvim",
-    "MunifTanjim/nui.nvim",
-    "kevinhwang91/promise-async",
+  {
+    'RicardoRamirezR/blade-nav.nvim'
   },
-  cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-  -- keys = {
-  --   { "<leader>la", ":Laravel artisan<cr>" },
-  --   { "<leader>lr", ":Laravel routes<cr>" },
-  --   { "<leader>lm", ":Laravel related<cr>" },
-  -- },
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "tpope/vim-dotenv",
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "kevinhwang91/promise-async",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    -- keys = {
+    --   { "<leader>la", ":Laravel artisan<cr>" },
+    --   { "<leader>lr", ":Laravel routes<cr>" },
+    --   { "<leader>lm", ":Laravel related<cr>" },
+    -- },
 
-  event = { "VeryLazy" },
-  opts = function (_, opts)
-    opts.features = {
-      route_info = {
-        enable = true,
-        position = "top",
+    event = { "VeryLazy" },
+    opts = function (_, opts)
+      opts.features = {
+        route_info = {
+          enable = true,
+          position = "top",
+        }
       }
-    }
 
-    opts.environments = {
-      definitions = {
-        {
-          name = "loop",
-          condition = {
-            file_exists = { "docker-compose.yml" },
-            executable = { "docker" },
-          },
-          commands = {
-            compose = { "docker", "compose" },
-            {
-              commands = { "php", "composer", "npm" },
-              docker = {
-                container = {
-                  env = "APP_SERVICE",
-                  default = "core",
+      opts.environments = {
+        definitions = {
+          {
+            name = "loop",
+            condition = {
+              file_exists = { "docker-compose.yml" },
+              executable = { "docker" },
+            },
+            commands = {
+              compose = { "docker", "compose" },
+              {
+                commands = { "php", "composer", "npm" },
+                docker = {
+                  container = {
+                    env = "APP_SERVICE",
+                    default = "core",
+                  },
+                  exec = { "docker", "compose", "exec", },
                 },
-                exec = { "docker", "compose", "exec", },
               },
             },
           },
-        },
-        {
-          name = "redwood",
-          condition = {
-            file_exists = { "docker-compose.yml" },
-            executable = { "docker" },
-          },
-          commands = {
-            compose = { "docker", "compose" },
-            {
-              commands = { "php", "composer" },
-              docker = {
-                container = {
-                  default = "redwood_api",
+          {
+            name = "redwood",
+            condition = {
+              file_exists = { "docker-compose.yml" },
+              executable = { "docker" },
+            },
+            commands = {
+              compose = { "docker", "compose" },
+              {
+                commands = { "php", "composer" },
+                docker = {
+                  container = {
+                    default = "redwood_api",
+                  },
+                  exec = { "docker", "compose", "exec", },
                 },
-                exec = { "docker", "compose", "exec", },
               },
             },
           },
-        },
+        }
       }
-    }
-  end,
+    end,
+  }
 }
