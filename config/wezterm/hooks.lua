1a9c57239431a2435d9f68c4473214e86edf90ee
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-03-11 16:43:51 -0500

    updating autosizing on super ultra wide

--- config/wezterm/hooks.lua
+++ config/wezterm/hooks.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 
 local padding = 32
-local menu_bar_height = 76
+local menu_bar_height = 25
 local laptop_menu_bar_height = 76
 
 local M = {}
@@ -17,13 +17,19 @@ function M.setup(_)
       local current_ratio = active_screen.width / active_screen.height
 
       if current_ratio >= monitor_ratio then
-        window:gui_window():set_position((active_screen.width) / 4 + padding, padding + menu_bar_height)
+        window:gui_window():set_position(
+          active_screen.width / 4 + padding,
+          padding + menu_bar_height
+        )
         window:gui_window():set_inner_size(
-          (active_screen.width - (padding * 2)) / 2,
-          active_screen.height - (padding * 2) - menu_bar_height
+          (active_screen.width - (padding * 4)) / 2,
+          active_screen.height - padding * 2 - menu_bar_height
         )
       else
-        window:gui_window():set_position(padding * 2, padding * 2 + laptop_menu_bar_height)
+        window:gui_window():set_position(
+          padding * 2,
+          padding * 2 + laptop_menu_bar_height
+        )
         window:gui_window():set_inner_size(
           (active_screen.width - padding * 4),
           active_screen.height - (padding * 4) - laptop_menu_bar_height
