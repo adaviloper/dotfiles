ac329b2196ebe84fb9674e9ed7adb93047cbc634
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-03-20 17:08:32 -0500

    removing work config settings

--- config/nvim/lua/plugins/php.lua
+++ config/nvim/lua/plugins/php.lua
@@ -6,29 +6,15 @@ return {
   {
     'adaviloper/php-dev-tools.nvim',
     -- dir = '~/Code/php-dev-tools.nvim',
-    ft = 'php',
+    -- ft = 'php',
+    event = 'VeryLazy',
     config = function ()
       require('php-dev-tools').setup({
         test_utils = {
           {
-            dir = '~/Code/loop-returns-app',
-            cmd = './script/tests',
-            -- cmd = 'docker exec -it core vendor/bin/phpunit --filter',
-            group_cmd = 'dock ./vendor/bin/phpunit --list-groups',
-            groups = {
-              'easyship',
-              'multi-label',
-              'shipping-protection',
-              'stripe'
-            },
-          },
-          {
             dir = '~/Code/redwood',
             cmd = 'dart test',
-          },
-          {
-            dir = '~/Code/figs-recycling-campaign/logistics',
-            cmd = 'sail test',
+            group_cmd = 'dock ./vendor/bin/phpunit --list-groups',
           },
         }
       })
