cb2708987c7469e6b9a84646d52be48bb31036cc
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-03-29 19:14:25 -0500

    updating php icon

--- config/nvim/lua/plugins/user.lua
+++ config/nvim/lua/plugins/user.lua
@@ -1,14 +1,12 @@
-local mocha = require('catppuccin.palettes').get_palette('mocha')
+local mocha = require("catppuccin.palettes").get_palette("mocha")
 
 -- You can also add new plugins here as well using the lazy syntax:
 return {
-  'andweeb/presence.nvim',
+  "andweeb/presence.nvim",
   {
-    'ray-x/lsp_signature.nvim',
-    event = 'BufRead',
-    config = function()
-      require('lsp_signature').setup()
-    end,
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
   },
 
   -- customize alpha options
@@ -17,12 +15,12 @@ return {
     opts = function(_, opts)
       -- customize the dashboard header
 
-      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo1', { fg = mocha.blue })
-      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo2', { fg = mocha.green, bg = mocha.blue })
-      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo2_1', { fg = mocha.blue, bg = mocha.green })
-      vim.api.nvim_set_hl(0, 'NeovimDashboardLogo3', { fg = mocha.green })
-      vim.api.nvim_set_hl(0, 'NeovimDashboardFooter1', { fg = mocha.blue })
-      vim.api.nvim_set_hl(0, 'NeovimDashboardFooter2', { fg = mocha.green })
+      vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = mocha.blue })
+      vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = mocha.green, bg = mocha.blue })
+      vim.api.nvim_set_hl(0, "NeovimDashboardLogo2_1", { fg = mocha.blue, bg = mocha.green })
+      vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = mocha.green })
+      vim.api.nvim_set_hl(0, "NeovimDashboardFooter1", { fg = mocha.blue })
+      vim.api.nvim_set_hl(0, "NeovimDashboardFooter2", { fg = mocha.green })
       opts.dashboard.preset.header = table.concat({
         [[     ██        ]],
         [[    ███        ]],
@@ -36,7 +34,6 @@ return {
         [[]],
         [[N  E  O  V  I  M]],
       }, "\n")
-
 
       -- opts.dashboard = {
       --   { { 'NeovimDashboardLogo1', 2, 45 } },
@@ -52,27 +49,39 @@ return {
       --   { { 'NeovimDashboardFooter1', 0, 8 }, { 'NeovimDashboardFooter2', 9, 22 } },
       -- }
 
+      -- require("snacks").picker.pick(nil, {
+      --   title = 'Groups',
+      --   live = true,
+      --   layout = 'select',
+      --   format = 'text',
+      --   finder = function()
+      --     return {
+      --       text = "hit",
+      --     }
+      --   end,
+      -- })
+
       return opts
-    end
+    end,
   },
 
   -- You can disable default plugins as follows:
-  { 'max397574/better-escape.nvim', enabled = false },
-  --
+  { "max397574/better-escape.nvim", enabled = false },
+
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
+      local npairs = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+      local cond = require("nvim-autopairs.conds")
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
             -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
+            :with_pair(cond.not_after_regex("%%"))
             -- don't add a pair if  the previous character is xxx
             :with_pair(
               cond.not_before_regex("xxx", 3)
@@ -80,7 +89,7 @@ return {
             -- don't move right when repeat character
             :with_move(cond.none())
             -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
+            :with_del(cond.not_after_regex("xx"))
             -- disable adding a newline when you press <cr>
             :with_cr(cond.none()),
         },
@@ -89,4 +98,13 @@ return {
       )
     end,
   },
+
+  {
+    'echasnovski/mini.icons',
+    opts = {
+      extension = {
+        php = {  glyph = '' },
+      },
+    }
+  }
 }
