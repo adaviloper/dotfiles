7ecf3e59fda5a5069264b4c3561ad5c0e3c16633
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-03-29 19:13:54 -0500

    updating path

--- config/nvim/lua/config/pickers/multigrep.lua
+++ config/nvim/lua/config/pickers/multigrep.lua
@@ -1,3 +1,4 @@
+local snacks = require("snacks")
 local finders = require('telescope.finders')
 local make_entry = require('telescope.make_entry')
 local pickers = require('telescope.pickers')
@@ -37,13 +38,35 @@ M.live_multigrep = function (opts)
     cwd = opts.cwd,
   })
 
-  pickers.new(opts, {
-    debounce = 100,
-    prompt_title = 'Live Multigrep',
+  -- pickers.new(opts, {
+  --   debounce = 100,
+  --   prompt_title = 'Live Multigrep',
+  --   finder = finder,
+  --   previewer = config.grep_previewer(opts),
+  --   sorter = require('telescope.sorters').empty()
+  -- }):find()
+
+  snacks.picker.pick(nil, {
+    title = 'Multi',
+    layout = {
+      preview = true,
+      layout = {
+        backdrop = false,
+        width = 0.5,
+        -- min_width = 80,
+        height = 0.4,
+        min_height = 3,
+        box = "vertical",
+        border = "rounded",
+        title = "{title}",
+        title_pos = "center",
+        { win = "input", height = 1, border = "bottom" },
+        { win = "list", border = "none" },
+        { win = "preview", title = "{preview}", height = 0.4, border = "top" },
+      },
+    },
     finder = finder,
-    previewer = config.grep_previewer(opts),
-    sorter = require('telescope.sorters').empty()
-  }):find()
+  })
 end
 
 return M
