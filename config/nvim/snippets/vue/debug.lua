82aeb050ceca2e2345b35239585b76389b09ba33
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-03-31 20:24:21 -0500

    updating snippet to align with PHP trigger

--- config/nvim/snippets/vue/debug.lua
+++ config/nvim/snippets/vue/debug.lua
@@ -1,18 +1,17 @@
 return
   {
     s(
-      'cml',
+      'dml',
       fmt(
-        [[console.{}('{}'{});]],
+        [[console.log('{}'{});]],
         {
-          c(1, { t('log'), t('error'), t('table') }),
           f(
             function ()
-              local line_number = vim.api.nvim_win_get_cursor(0)
+              local line_number = vim.api.nvim_win_get_cursor(-1)
               return vim.fn.expand('%:t') .. ':' .. line_number[1]
             end
           ),
-          i(2, '')
+          i(1, '')
         }
       )
     )
