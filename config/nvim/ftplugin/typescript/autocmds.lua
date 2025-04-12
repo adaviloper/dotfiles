493756cd66f06d4164a4b747677051f70387a463
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-04-12 16:33:10 -0500

    adding Vue3 file templates

--- config/nvim/ftplugin/typescript/autocmds.lua
+++ config/nvim/ftplugin/typescript/autocmds.lua
@@ -1,3 +1,4 @@
+local file_utils = require "helpers.file_utils"
 local ts_group = vim.api.nvim_create_augroup('TypeScript Autotemplates', { clear = true })
 
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
@@ -26,28 +27,68 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+vim.api.nvim_create_autocmd({ 'BufNew', 'BufNewFile' }, {
   pattern = '*/store/*.ts',
   group = ts_group,
-  callback = function (params)
-    require('config.commands.tpl').insert_template({
-      "import { defineStore } from 'pinia'",
-      "",
-      "interface State {",
-      "",
-      "}",
-      "",
-      "export const useMainStore = defineStore('main', {",
-      "  state: (): State => ({",
-      "  }),",
-      "",
-      "  actions: {",
-      "  },",
-      "",
-      "  getters: {",
-      "  },",
-      "})",
-    })
-    vim.api.nvim_win_set_cursor(0, { 4, 1 })
+  callback = function (args)
+    if args.file:match(".*/store/%a+%.ts") then
+      vim.notify(vim.inspect(args.file))
+      vim.api.nvim_buf_set_lines(args.buf, 0, -1, false, {
+        "import { defineStore } from 'pinia'",
+        "",
+        "type State = {",
+        "",
+        "};",
+        "",
+        "export const useMainStore = defineStore('main', {",
+        "  state: (): State => ({",
+        "  }),",
+        "",
+        "  actions: {",
+        "  },",
+        "",
+        "  getters: {",
+        "  },",
+        "})",
+      })
+
+      vim.cmd([[w]])
+      vim.cmd([[e]])
+    end
   end,
 })
+
+
+vim.api.nvim_create_autocmd({ 'BufNew', 'BufNewFile' }, {
+  pattern = '*/router/*.ts',
+  group = ts_group,
+  callback = function (args)
+    if args.file:match(".*/router/%a+%.ts") then
+      vim.api.nvim_buf_set_lines(args.buf or 0, 0, -1, false, {
+        "import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'",
+        "import HelloWorld from '@/components/HelloWorld.vue';",
+        "",
+        "const routes: RouteRecordRaw[] = [",
+        "  {",
+        "    path: '/',",
+        "    component: HelloWorld",
+        "  },",
+        "];",
+        "",
+        "const router = createRouter({",
+        "  history: createWebHistory(),",
+        "  routes,",
+        "});",
+        "",
+        "export {",
+        "  router,",
+        "}",
+        "",
+      })
+
+      vim.cmd([[w]])
+      vim.cmd([[e]])
+    end
+  end,
+})
+
