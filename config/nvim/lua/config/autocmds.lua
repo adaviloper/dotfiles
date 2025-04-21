e08561ab74cf68eb7bc9b889221aecc1657cdc47
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-04-20 20:48:58 -0500

    moving php autocmd config to ftplugin path

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,3 @@
-local php_group = vim.api.nvim_create_augroup('PHP AutoTemplates', { clear = true })
-
 vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('adaviloper/close_with_q', { clear = true }),
   desc = 'Close with <q>',
@@ -17,46 +15,7 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
-vim.api.nvim_create_autocmd({ 'LspAttach' }, {
-  pattern = '*.php',
-  group = php_group,
-  callback = function (params)
-    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-    if #lines == 1 and lines[1] == '' then
-      local client = vim.lsp.get_client_by_id(params.data.client_id)
-      if client == nil then return end
-      local file = vim.fn.expand("%:p:.")
-      local match_position = -1
-      local target_type = 'default'
-      for _, type in ipairs({ 'Enum', 'Feature', 'Unit', 'Jobs', 'Listeners', 'Models' }) do
-        local pos = file:find(type)
-        if pos ~= nil then
-          if match_position == -1 or pos < match_position then
-            target_type = type
-            match_position = pos
-          end
-        end
-      end
-
-      if client.supports_method('textDocument/codeAction') then
-        vim.lsp.buf.code_action({
-          filter = function (ca)
-            if ca.command ~= nil and ca.command.title ~= nil then
-              if ca.command.title:find(target_type) ~= nil then
-                return true
-              end
-            end
-          end,
-          apply = true
-        })
-      else
-        vim.api.nvim_buf_set_lines(0, 0, -1, false, { '<?php', '', '' })
-      end
-    end
-  end,
-})
-
-local win_group = vim.api.nvim_create_augroup('Split Manager', { clear = true })
+local win_group = vim.api.nvim_create_augroup('adaviloper/split_right', { clear = true })
 
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   group = win_group,
@@ -64,7 +23,7 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   command = 'wincmd L',
 })
 
-local misc_group = vim.api.nvim_create_augroup('Miscellaneous', { clear = true })
+local misc_group = vim.api.nvim_create_augroup('adaviloper/miscellaneous', { clear = true })
 
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   group = misc_group,
