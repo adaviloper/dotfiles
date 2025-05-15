481bd69c3387849140fd9784fb8d4d5e24114bbc
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-05-15 18:42:18 -0500

    restructuring str_utils

--- config/nvim/lua/helpers/str_utils.lua
+++ config/nvim/lua/helpers/str_utils.lua
@@ -1,6 +1,27 @@
 local M = {}
 
-function firstToUpper(str) return (str:lower():gsub("^%l", string.upper)) end
+local gsub = string.gsub
+local lower = string.lower
+local upper = string.upper
+
+local function trim(str)
+  return gsub(str, "^%s*", ''):gsub("%s*$", '')
+end
+
+local function strip_special_characters(str)
+  local special_characters = { "_", "-", "'" }
+  str = trim(str)
+
+  for _, character in ipairs(special_characters) do
+    str = gsub(str, character, " ")
+  end
+
+  return str
+end
+
+local function firstToUpper(str)
+  return gsub(lower(str), "^%l", upper)
+end
 
 function M.title_case(str)
   local name_parts = {}
@@ -14,27 +35,22 @@ function M.title_case(str)
   return table.concat(name_parts, " ")
 end
 
-function strip_special_characters(str)
-  local special_characters = { "_", "-", "'" }
-  str = str:gsub("^%s*", ''):gsub("%s*$", '')
-
-  for _, character in ipairs(special_characters) do
-    str = str:gsub(character, " ")
-  end
-
-  return str
-end
-
 function M.slug(str)
   str = strip_special_characters(str):gsub(" ", "-"):lower()
-
   return str
 end
 
 function M.snake(str)
-  str = strip_special_characters(str):gsub("-", "_")
-
+  str = strip_special_characters(str):gsub("-", "_"):lower()
   return str
 end
 
+function M.camel(str)
+  str = strip_special_characters(str)
+  return str
+    :gsub(" (%w)", function(c) return c:upper() end)
+    :gsub("^%u", lower)
+end
+
 return M
+
