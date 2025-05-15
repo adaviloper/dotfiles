9e2ae7b4e2a0e467b8106d68b1625873546802f0
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-05-14 19:00:09 -0500

    tweaking ubuntu install script

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -18,37 +18,69 @@ info() {
 info "Updating package lists"
 sudo apt update
 
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+
+sudo apt update
+
 # Install required dependencies early
 info "Installing core packages"
 sudo apt install -y \
-  gnome-tweaks \
+  build-essential \
+  gnome-browser-connector \
   gnome-shell-extensions \
-  libwxgtk3.0-gtk3-dev \
+  gnome-tweaks \
   curl \
-  wget
+  gcc \
+  git \
+  libwxgtk3.2-dev \
+  wezterm \
+  wget \
+  zsh
 
 # Install Homebrew (Linux)
-/home/linuxbrew/.linuxbrew/bin/brew --version &>/dev/null || {
-  info "Installing Homebrew"
+info "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-}
 
 # Setup Homebrew env
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Run Brewfile
-if [[ -f "$HOME/.dotfiles/Brewfile" ]]; then
+if [ -f "$HOME/.dotfiles/Brewfile" ]; then
   info "Running Brewfile"
   brew bundle --file="$HOME/.dotfiles/Brewfile"
 else
   info "No Brewfile found at ~/.dotfiles/Brewfile"
 fi
 
+# Oh-My-Zsh installation
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  info "Installing Oh-My-Zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+fi
+
+# Run rcup if available
+if command -v rcup &>/dev/null; then
+  info "Running rcup"
+  rcup -f
+
+  # TPM for tmux
+  TPM_DIR="$HOME/.config/tmux/plugins/tpm"
+  if [ ! -d "$TPM_DIR" ]; then
+    info "Cloning TPM (tmux plugin manager)"
+    git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+  fi
+else
+  info "rcup not found, skipping dotfile linking"
+fi
+
 # Yazi plugins (if ya is available)
 if command -v ya &>/dev/null; then
   info "Installing Yazi plugins"
-  ya pack -a yazi-rs/plugins:git
-  ya pack -a yazi-rs/flavors:catppuccin-mocha
+  # ya pack -a yazi-rs/plugins:git
+  # ya pack -a yazi-rs/flavors:catppuccin-mocha
 else
   info "Skipping Yazi plugin install (ya not found)"
 fi
@@ -62,31 +94,9 @@ tmux source-file "$HOME/.config/tmux/tmux.conf"
 tmux kill-session -t __tpm_temp
 
 # Run Go dependencies setup
-if [[ -f "$HOME/.dotfiles/scripts/go-dependencies.sh" ]]; then
+if [ -f "$HOME/.dotfiles/scripts/go-dependencies.sh" ]; then
   info "Running go-dependencies.sh"
   bash "$HOME/.dotfiles/scripts/go-dependencies.sh"
-fi
-
-# Oh-My-Zsh installation
-if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-  info "Installing Oh-My-Zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-fi
-
-# Run rcup if available
-if command -v rcup &>/dev/null; then
-  info "Running rcup"
-  rcup -f
-
-  # TPM for tmux
-  TPM_DIR="$HOME/.config/tmux/plugins/tpm"
-  if [[ ! -d "$TPM_DIR" ]]; then
-    info "Cloning TPM (tmux plugin manager)"
-    git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
-  fi
-else
-  info "rcup not found, skipping dotfile linking"
 fi
 
 # Prepare notes directory
@@ -98,30 +108,29 @@ if command -v nvim &>/dev/null; then
   nvim --headless +q
 fi
 
-# Change shell to zsh if not already
-if [[ "$SHELL" != "$(which zsh)" ]]; then
-  info "Changing default shell to zsh"
-  chsh -s "$(which zsh)"
-fi
-
 # Espanso install
-ESPANSO_DEB="espanso-debian-wayland-amd64.deb"
-if ! command -v espanso &>/dev/null; then
-  info "Installing Espanso"
-  wget "https://github.com/espanso/espanso/releases/download/v2.2.1/${ESPANSO_DEB}"
-  sudo apt install -y "./${ESPANSO_DEB}"
-  rm "${ESPANSO_DEB}"
-  sudo setcap "cap_dac_override+p" "$(which espanso)"
-  espanso service register
-fi
+# ESPANSO_DEB="espanso-debian-wayland-amd64.deb"
+# if ! command -v espanso &>/dev/null; then
+#   info "Downloading Espanso"
+#   wget "https://github.com/espanso/espanso/releases/download/v2.2.1/${ESPANSO_DEB}"
+#   info "Installing Espanso"
+#   sudo apt install -y "./${ESPANSO_DEB}"
+#   info "Removing Espanso install script"
+#   rm "${ESPANSO_DEB}"
+#   sudo setcap "cap_dac_override+p" "$(which espanso)"
+#   info "Registering Espanso service"
+#   espanso service register
+# fi
 
 # GNOME Shell Extensions
 EXT_DIR="$HOME/.local/share/gnome-shell/extensions"
+info "Creating extensions path"
+mkdir -p "$EXT_DIR"
 
 clone_extension() {
   local repo="$1"
   local dir="$2"
-  if [[ ! -d "$EXT_DIR/$dir" ]]; then
+  if [ ! -d "$EXT_DIR/$dir" ]; then
     info "Cloning GNOME extension: $dir"
     git clone "git@github.com:$repo.git" "$EXT_DIR/$dir"
   fi
@@ -133,3 +142,10 @@ clone_extension "adaviloper/quicker" "quicker@adaviloper.com"
 
 info "Setup complete."
 
+# Change shell to zsh if not already
+if [ "$SHELL" != "$(which zsh)" ]; then
+  info "Changing default shell to zsh"
+  chsh -s "$(which zsh)"
+fi
+
+sudo apt autoremove
