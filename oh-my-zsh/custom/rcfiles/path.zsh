a355ceb65f17bb990917262af90aa02eaf9ae128
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-05-20 21:35:04 -0500

    adding .local/bin to path

--- oh-my-zsh/custom/rcfiles/path.zsh
+++ oh-my-zsh/custom/rcfiles/path.zsh
@@ -1,2 +1,7 @@
-export PATH="/Applications/PhpStorm.app/Contents/bin:/opt/homebrew/opt/avr-gcc@8/bin:$HOME/.config/bin/docker:$HOME/.config/bin/git:$HOME/.config/bin:$HOME/go/bin:/opt/homebrew/opt/arm-none-eabi-gcc@8/bin:/opt/homebrew/opt/arm-gcc-bin@8/bin:$HOME/.local/share/nvim/mason/bin:Appyications/PhpStorm.app/Contents/bin:/Applications/XAMPP/bin:yarn global bin:$HOME/.luarocks/bin:$PATH"
+export PATH="/Applications/PhpStorm.app/Contents/bin:/opt/homebrew/opt/avr-gcc@8/bin:$HOME/.config/bin/docker:$HOME/.config/bin/git:$HOME/.config/bin:$HOME/go/bin:/opt/homebrew/opt/arm-none-eabi-gcc@8/bin:/opt/homebrew/opt/arm-gcc-bin@8/bin:$HOME/.local/bin:$HOME/.local/share/nvim/mason/bin:Appyications/PhpStorm.app/Contents/bin:/Applications/XAMPP/bin:yarn global bin:$HOME/.luarocks/bin:$PATH"
 export PATH="/home/linuxbrew/.linuxbrew/bin/:$PATH"
+
+export XDG_DATA_DIRS="$HOME/.local/xdg:$XDG_DATA_DIRS"
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:$XDG_DATA_DIRS"
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/bin:$XDG_DATA_DIRS"
+export XDG_DATA_DIRS="$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
