283eb6bf6ea54966054d249a9ba103b36578348e
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-05-30 14:59:30 -0500

    organizing norg keybindings

--- config/nvim/ftplugin/norg/keybindings.lua
+++ config/nvim/ftplugin/norg/keybindings.lua
@@ -4,12 +4,12 @@ local neorg_templates = require("helpers.neorg_templates")
 astrocore.set_mappings({
   n = {
     ['<A-Space>'] = { '<Plug>(neorg.qol.todo-items.todo.task-cycle)', desc = 'Increment task state' },
-    ['<Leader>n'] = { name = "󱓩 Notes" },
-    ['<LocalLeader>c'] = { function () neorg_templates.createCompany() end, desc = 'Create a new company note' },
-    ['<LocalLeader>d'] = { function () neorg_templates.createDailyEntry() end, desc = 'Create a daily note' },
-    ['<LocalLeader>m'] = { function () neorg_templates.createMeetingNote() end, desc = 'Create a meeting note' },
-    ['<LocalLeader>p'] = { function () neorg_templates.createPerson() end, desc = 'Create a new person note' },
-    ['<LocalLeader>w'] = { function () neorg_templates.createWeeklyEntry() end, desc = 'Create a weekly note' },
+    ['<LocalLeader>n'] = { name = "󱓩 Notes" },
+    ['<LocalLeader>nc'] = { function () neorg_templates.createCompany() end, desc = 'Create a new company note' },
+    ['<LocalLeader>nd'] = { function () neorg_templates.createDailyEntry() end, desc = 'Create a daily note' },
+    ['<LocalLeader>nm'] = { function () neorg_templates.createMeetingNote() end, desc = 'Create a meeting note' },
+    ['<LocalLeader>np'] = { function () neorg_templates.createPerson() end, desc = 'Create a new person note' },
+    ['<LocalLeader>nw'] = { function () neorg_templates.createWeeklyEntry() end, desc = 'Create a weekly note' },
 
     ['<LocalLeader>t'] = {
       '<Cmd>Neorg toc<CR>',
