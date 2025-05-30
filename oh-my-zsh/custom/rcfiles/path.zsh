140de8ff00a3db9aa50c2b72d12cf120561f0ddd
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-05-30 15:02:29 -0500

    adding docker cli to path

--- oh-my-zsh/custom/rcfiles/path.zsh
+++ oh-my-zsh/custom/rcfiles/path.zsh
@@ -1,5 +1,6 @@
 export PATH="/Applications/PhpStorm.app/Contents/bin:/opt/homebrew/opt/avr-gcc@8/bin:$HOME/.config/bin/docker:$HOME/.config/bin/git:$HOME/.config/bin:$HOME/go/bin:/opt/homebrew/opt/arm-none-eabi-gcc@8/bin:/opt/homebrew/opt/arm-gcc-bin@8/bin:$HOME/.local/bin:$HOME/.local/share/nvim/mason/bin:Appyications/PhpStorm.app/Contents/bin:/Applications/XAMPP/bin:yarn global bin:$HOME/.luarocks/bin:$PATH"
 export PATH="/home/linuxbrew/.linuxbrew/bin/:$PATH"
+export PATH="$HOME/.docker/cli-plugins:$PATH"
 
 export XDG_DATA_DIRS="$HOME/.local/xdg:$XDG_DATA_DIRS"
 export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:$XDG_DATA_DIRS"
