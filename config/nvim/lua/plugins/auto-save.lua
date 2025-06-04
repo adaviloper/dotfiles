503afea9d1224ef2c5b39ca020dc5bc53568356c
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-06-04 11:12:26 -0500

    decreasing autosave debounce delay

--- config/nvim/lua/plugins/auto-save.lua
+++ config/nvim/lua/plugins/auto-save.lua
@@ -4,6 +4,6 @@ return {
   opts = {
     enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
     write_all_buffers = true, -- write all buffers when the current one meets `condition`
-    debounce_delay = 3000, -- delay after which a pending save is executed
+    debounce_delay = 2000, -- delay after which a pending save is executed
   },
 }
