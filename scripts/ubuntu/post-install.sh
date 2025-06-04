13ef078971066ac0522b8beae9f133de030f124b
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-06-04 11:13:17 -0500

    adding headset controls

--- scripts/ubuntu/post-install.sh
+++ scripts/ubuntu/post-install.sh
@@ -2,6 +2,7 @@
 
 flatpak install flathub app.zen_browser.zen
 
+# Espanso
 sudo apt update && sudo apt install -y build-essential git wl-clipboard libxkbcommon-dev libdbus-1-dev '^libwxgtk3\.[0-9].*-dev$' libssl-dev
 sudo apt install -y cargo
 cd $HOME/Code/
@@ -16,4 +17,15 @@ sudo mv target/release/espanso /usr/local/bin/espanso
 sudo setcap "cap_dac_override+p" $(which espanso)
 espanso service register
 espanso start
+
+# Headset controls
+sudo apt-get install -y build-essential git cmake libhidapi-dev
+git clone https://github.com/Sapd/HeadsetControl "$HOME/Code/HeadsetControl" && cd "$HOME/Code/HeadsetControl"
+mkdir build && cd build
+cmake ..
+make
+sudo make install
+
+# On LINUX, to access without root reboot your computer or run
+sudo udevadm control --reload-rules && sudo udevadm trigger
 
