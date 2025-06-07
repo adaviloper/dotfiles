6b5df6bb4486ae582fa2491da13f43ec85936b55
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-06-07 16:40:16 -0500

    adding docker instructions

--- scripts/ubuntu/post-install.sh
+++ scripts/ubuntu/post-install.sh
@@ -18,14 +18,35 @@ sudo setcap "cap_dac_override+p" $(which espanso)
 espanso service register
 espanso start
 
-# Headset controls
+# --- START HEADSET CONTROLS
 sudo apt-get install -y build-essential git cmake libhidapi-dev
 git clone https://github.com/Sapd/HeadsetControl "$HOME/Code/HeadsetControl" && cd "$HOME/Code/HeadsetControl"
 mkdir build && cd build
 cmake ..
 make
 sudo make install
-
 # On LINUX, to access without root reboot your computer or run
 sudo udevadm control --reload-rules && sudo udevadm trigger
+# --- END HEADSET CONTROLS
 
+# --- START DOCKER
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# // /etc/docker/daemon.json
+# {
+#   "dns": ["8.8.8.8", "1.1.1.1"]
+# }
+
+# --- END DOCKER
