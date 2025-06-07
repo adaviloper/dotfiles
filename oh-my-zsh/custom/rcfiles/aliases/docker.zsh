a52dc2e139250c54304624ce006ef14cdf08aafd
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-06-07 16:40:02 -0500

    updating to use docker compose

--- oh-my-zsh/custom/rcfiles/aliases/docker.zsh
+++ oh-my-zsh/custom/rcfiles/aliases/docker.zsh
@@ -1,5 +1,5 @@
-alias dd="docker-compose down"
-alias dup="docker-compose up -d"
+alias dd="docker compose down"
+alias dup="docker compose up -d"
 
 alias dlogs="docker-compose logs -f"
 alias detonate="docker exec -it core /bin/bash -c 'APP_ENV=testing php artisan migrate:fresh --seeder TestSeeder'"
