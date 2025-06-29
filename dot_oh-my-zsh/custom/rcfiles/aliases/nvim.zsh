545c47fbfa619d183e14ba01477c56b9fa6832b3
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-06-29 14:32:29 -0500

    adding function for dynamically swapping nvim configs

--- dot_oh-my-zsh/custom/rcfiles/aliases/nvim.zsh
+++ dot_oh-my-zsh/custom/rcfiles/aliases/nvim.zsh
@@ -1,1 +1,11 @@
 alias ts_norg="CC=/opt/homebrew/opt/llvm/bin/clang nvim --headless -c 'TSInstallSync norg'"
+alias nvim-kickstart='NVIM_APPNAME="nvim-kickstart" nvim'
+
+DEFAULT="nvim"
+
+function nv() {
+  CONFIGURATION=$(gum choose 'nvim-kickstart' $DEFAULT)
+  export NVIM_APPNAME=$CONFIGURATION
+
+  nvim
+}
