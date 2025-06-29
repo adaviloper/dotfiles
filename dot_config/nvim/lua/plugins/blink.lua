a7c13451e8c65dfcb17c224f7be725a0bffb67d8
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-06-29 14:32:02 -0500

    updating blink keymaps

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,17 +1,32 @@
 return {
---  "Saghen/blink.cmp",
---  opts = {
---    keymap = {
---      ["<Up>"] = { "select_prev", "fallback" },
---      ["<Down>"] = { "select_next", "fallback" },
---      ["<C-n>"] = { "select_next", "show" },
---      ["<C-p>"] = { "select_prev", "show" },
---      ["<C-j>"] = {},
---      ["<C-k>"] = {},
---      ["<C-U>"] = { "scroll_documentation_up", "fallback" },
---      ["<C-D>"] = { "scroll_documentation_down", "fallback" },
---      ["<C-e>"] = { "hide", "fallback" },
---      -- ["<C-y>"] = { "accept", "fallback" },
---    },
---  },
+  "Saghen/blink.cmp",
+  opts = {
+    -- preset = 'none',
+    keymap = {
+      ["<Up>"] = false,
+      ["<Down>"] = false,
+      ["<C-n>"] = { "select_next", "show" },
+      ["<C-p>"] = { "select_prev", "show" },
+      ["<C-j>"] = { 'snippet_forward', 'fallback' },
+      ["<C-k>"] = { 'snippet_backward', 'fallback' },
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<C-y>"] = { "accept", "fallback" },
+      ["<C-i>"] = { "show_signature", "hide_signature", "fallback" },
+      ["<Tab>"] = false,
+      ["<S-Tab>"] = false,
+      s(
+        "i",
+        fmt([[
+        test{}
+        ]],
+        {
+          
+        }
+        )
+      )
+    },
+  },
 }
+
