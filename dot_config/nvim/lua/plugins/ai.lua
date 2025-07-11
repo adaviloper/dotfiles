ea6b5d85d07c63c01321496ad0a9792ab22f438b
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-07-11 10:27:42 -0500

    disabling ai plugin

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,8 +1,24 @@
 return {
-  -- {
-  --   'github/copilot.vim',
-  --   config = function ()
-  --     vim.g.copilot_no_tab_map = true
-  --   end
-  -- },
+  {
+    'zbirenbaum/copilot.lua',
+    enabled = false,
+    config = function ()
+      require('copilot').setup({
+        filetypes = {
+          javascript = true,
+          typescript = true,
+          php = true,
+          sh = true,
+          ["*"] = false, -- Keeping a whitelist of allowed filetypes where Copilot is allowed
+        },
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "BlinkCmpMenuClose",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = false
+        end,
+      })
+    end
+  },
 }
