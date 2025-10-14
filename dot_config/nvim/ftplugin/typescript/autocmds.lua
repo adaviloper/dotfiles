b69ba298442ab137012a9457e2cf4c77ad4c7e20
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-10-13 19:17:45 -0500

    removing overreaching autocommand

--- dot_config/nvim/ftplugin/typescript/autocmds.lua
+++ dot_config/nvim/ftplugin/typescript/autocmds.lua
@@ -1,32 +1,6 @@
 local file_utils = require "helpers.file_utils"
 local ts_group = vim.api.nvim_create_augroup('TypeScript Autotemplates', { clear = true })
 
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-  pattern = '*/2024/*.ts',
-  group = ts_group,
-  callback = function (params)
-    local file_name = vim.fn.expand('%:t:r')
-    -- vim.cmd('!touch ./data/' .. file_name .. '.data.ts')
-    require('config.commands.tpl').insert_template({
-      "import { input } from './data/" .. file_name .. ".data.ts';",
-      "",
-      "const env = 'prod';",
-      "const data = input[env];",
-      "",
-      "const p1 = () => {",
-      "  ",
-      "}",
-      "",
-      "const p2 = () => {",
-      "}",
-      "",
-      "console.log('p1', p1())",
-      "console.log('p2', p2())",
-    })
-    vim.api.nvim_win_set_cursor(0, { 7, 1 })
-  end,
-})
-
 vim.api.nvim_create_autocmd({ 'BufNew', 'BufNewFile' }, {
   pattern = '*/store/*.ts',
   group = ts_group,
