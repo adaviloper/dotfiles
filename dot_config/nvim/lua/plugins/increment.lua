69e7f46c6079a57297942f9d952a080c0e05a03a
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-10-29 13:03:55 -0500

    switching to increment.nvim

---
+++ dot_config/nvim/lua/plugins/increment.lua
@@ -0,0 +1,38 @@
+return {
+  'Ipomoea/increment.nvim',
+  enabled = true,
+  config = function()
+    require('increment').setup({
+      -- mappings = {
+      --   increment = '<C-a>',
+      --   decrement = '<C-x>'
+      -- },
+      -- User defined loops
+      groups = {
+        {'get', 'set'},
+        {'up', 'down'},
+        {'true', 'false'},
+        {'==', '==='},
+        {'!=', '!=='},
+      },
+      lang_groups = {
+        php = {
+          {'dd', 'dump'},
+          {'protected', 'public', 'private'},
+        },
+        javascript = {
+          {'let', 'const', 'var'},
+          {'skip', 'only'},
+        },
+        typescript = {
+          {'let', 'const', 'var'},
+          {'skip', 'only'},
+        },
+        sh = {
+          {'production', 'development', 'local', 'testing'},
+        }
+      },
+    })
+  end,
+  event = { "BufReadPost" }, -- lazy load after reading a buffer
+}
