561db9aafe095cee1e5f48283dd7867244d40249
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-11-11 19:04:58 -0600

    adding quick test for current ts file

--- dot_config/nvim/ftplugin/typescript/keybindings.lua
+++ dot_config/nvim/ftplugin/typescript/keybindings.lua
@@ -3,6 +3,10 @@ local astrocore = require("astrocore")
 astrocore.set_mappings({
   n = {
     ['<LocalLeader>ts'] = { "<cmd>ToggleSyntax<CR>", desc = "Toggle arrow function syntax" },
+    ['<F4><F4>'] = { function ()
+      local file = vim.fn.expand("%:p:.")
+      vim.cmd('TermExec cmd="nrt ' .. file .. '"')
+    end, desc = "Toggle arrow function syntax" },
   },
   v = {
     ['<LocalLeader>xv'] = { '<cmd>Extract variable<cr>', desc = "Extract to a variable" },
