f2f68b5a97305e9825480fe3ecfe47eb83b12bea
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-12-10 13:15:02 -0600

    adding descriptions

--- dot_config/nvim/snippets/norg/links.lua
+++ dot_config/nvim/snippets/norg/links.lua
@@ -1,26 +1,34 @@
 local snippet_utils = require("helpers.snippet_utils")
 
-return
-  {
-    s(
-      'person',
-      fmt(
-        [=[{{:$/people/{}.norg:}}[{}]]=],
-        {
-          f(snippet_utils.slug_case_var, { 1 }),
-          i(1),
-        }
-      )
-    ),
-    s(
-      'service',
-      fmt(
-        [=[{{:$/services/{}.norg:}}[{}]]=],
-        {
-          f(snippet_utils.slug_case_var, { 1 }),
-          i(1),
-        }
-      )
-    ),
-  },
-  {}
+return {
+  s(
+    {
+      trig = "person",
+      desc = "Link a person",
+    },
+    fmt([=[{{:$/people/{}.norg:}}[{}]]=], {
+      f(snippet_utils.slug_case_var, { 1 }),
+      i(1),
+    })
+  ),
+  s(
+    {
+      trig = "service",
+      desc = "Link a service",
+    },
+    fmt([=[{{:$/services/{}.norg:}}[{}]]=], {
+      f(snippet_utils.slug_case_var, { 1 }),
+      i(1),
+    })
+  ),
+  s(
+    {
+      trig = "team",
+      desc = "Link a team",
+    },
+    fmt([=[{{:$/services/{}.norg:}}[{}]]=], {
+      f(snippet_utils.slug_case_var, { 1 }),
+      i(1),
+    })
+  ),
+}, {}
