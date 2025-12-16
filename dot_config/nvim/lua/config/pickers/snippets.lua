847e017b6a2975de4c6f26c76cb07c0f3685abc4
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-12-16 15:11:39 -0600

    abstracting luasnip utils

--- dot_config/nvim/lua/config/pickers/snippets.lua
+++ dot_config/nvim/lua/config/pickers/snippets.lua
@@ -1,5 +1,6 @@
 local Snacks = require "snacks"
 local ls = require('luasnip')
+local snippet_utils = require("helpers.snippet_utils")
 
 local M = {}
 
@@ -7,9 +8,6 @@ function format_snippets(available_snippets)
   local items = {}
   for _, snippets in pairs(available_snippets) do
     for _, snippet in ipairs(snippets) do
-      if snippet.id == 34 then
-        vim.notify('snippets.lua:10: ' .. vim.inspect(snippet.description[1]))
-      end
       table.insert(items, {
         text = snippet.trigger .. ': ' .. snippet.description[1] or snippet.trigger,
         key = snippet.trigger,
@@ -23,19 +21,9 @@ function format_snippets(available_snippets)
 end
 
 function M.view_snippets()
-  local available_snippets = ls.available(function (snip)
-	  return {
-	    id = snip.id,
-		  name = snip.name,
-		  trigger = snip.trigger,
-		  description = snip.description,
-		  wordTrig = snip.wordTrig and true or false,
-		  regTrig = snip.regTrig and true or false,
-	  }
-  end)
   Snacks.picker.pick({
     source = "snippets",
-    items = format_snippets(available_snippets),
+    items = format_snippets(snippet_utils.available_snippets()),
     format = "text",
     layout = {
       preview = false,
@@ -45,7 +33,6 @@ function M.view_snippets()
       },
     },
     confirm = function (picker, item)
-      vim.notify('snippets.lua:27: ' .. vim.inspect(item.trigger))
       picker:close()
       ls.snip_expand(item.value)
       vim.cmd('norm a')
--- dot_config/nvim/lua/helpers/snippet_utils.lua
+++ dot_config/nvim/lua/helpers/snippet_utils.lua
@@ -26,5 +26,45 @@ function M.title_case_var(args, _)
   return str_utils.title_case(text)
 end
 
+--- @class Snippet
+--- @field id integer
+--- @field name string
+--- @field trigger string
+--- @field description string
+--- @field wordTrig string
+--- @field regTrig string
+
+--- @return Snippet
+function M.available_snippets()
+  return ls.available(function (snip)
+	  return {
+	    id = snip.id,
+		  name = snip.name,
+		  trigger = snip.trigger,
+		  description = snip.description,
+		  wordTrig = snip.wordTrig and true or false,
+		  regTrig = snip.regTrig and true or false,
+	  }
+  end)
+end
+
+--- @return LuaSnip.Snippet
+function M.find_snippet_by_trigger(trigger)
+  local target_snippet = nil
+  for _, snippets in pairs(M.available_snippets()) do
+    for _, snippet in ipairs(snippets) do
+      if snippet.trigger == trigger then
+        target_snippet = snippet
+        break
+      end
+    end
+  end
+
+  if target_snippet == nil then return end
+
+  local id = target_snippet.id
+  return ls.get_id_snippet(id)
+end
+
 return M
 
