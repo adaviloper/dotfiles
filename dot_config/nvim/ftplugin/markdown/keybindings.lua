6d75952a42e877e8e8d0123149373f54b5f06be8
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-12-30 09:39:59 -0600

    refactoring notes plugins

---
+++ dot_config/nvim/ftplugin/markdown/keybindings.lua
@@ -0,0 +1,29 @@
+local astrocore = require("astrocore")
+
+astrocore.set_mappings({
+  n = {
+    -- ['<LocalLeader>cn'] = { '<Plug>(neorg.qol.todo-items.todo.task-cycle)', desc = 'Increment task state' },
+    -- ['<LocalLeader>n'] = { name = "󱓩 Notes" },
+    -- ['<LocalLeader>nc'] = { function () neorg_templates.createCompany() end, desc = 'Create a new company note' },
+    ['<LocalLeader>nd'] = { "<Cmd>Obsidian today<CR>", desc = 'Create a daily note' },
+    -- ['<LocalLeader>nm'] = { function () neorg_templates.createMeetingNote() end, desc = 'Create a meeting note' },
+    -- ['<LocalLeader>no'] = { function () neorg_templates.createOneOnOneNote() end, desc = 'Create a One-on-One note' },
+    -- ['<LocalLeader>np'] = { function () neorg_templates.createPerson() end, desc = 'Create a new person note' },
+    -- ['<LocalLeader>nt'] = { function () neorg_templates.createTicket() end, desc = 'Create a ticket note' },
+    -- ['<LocalLeader>nw'] = { function () neorg_templates.createWeeklyEntry() end, desc = 'Create a weekly note' },
+
+    ['<LocalLeader>gt'] = {
+      '<Cmd>e to-do.norg<CR>',
+      desc = 'Open To Do list'
+    },
+
+    ['<LocalLeader>t'] = {
+      '<Cmd>Neorg toc<CR>',
+      desc = 'Open Table of Contents'
+    },
+  },
+
+  i = {
+    ['<A-CR>'] = { '<Plug>(neorg.itero.next-iteration)', desc = 'Drop into next iteration' },
+  },
+})
---
+++ dot_config/nvim/lua/plugins/notes.lua
@@ -0,0 +1,155 @@
+local neorg_templates = require("helpers.neorg_templates")
+local ls = require('luasnip')
+local t = ls.text_node
+local c = ls.choice_node
+
+return {
+    {
+      "obsidian-nvim/obsidian.nvim",
+      version = "*", -- recommended, use latest release instead of latest commit
+      ft = "markdown",
+      ---@module 'obsidian'
+      ---@type obsidian.config
+      opts = {
+        legacy_commands = false, -- this will be removed in the next major release
+        workspaces = {
+          {
+            name = "personal",
+            path = "~/Code/notes/obsidian/personal",
+          },
+          {
+            name = "work",
+            path = "~/Code/notes/obsidian/work",
+          },
+        },
+        daily_notes = {
+          folder = 'journal',
+          -- date_format = '%Y/%m/%Y-%m-%d',
+        },
+      note_id_func = function ()
+        local year, month, date = os.date("%Y"), os.date("%m"), os.date("%Y-%m-%d")
+        return string.format("/%s/%s/%s", year, month, date)
+      end
+      },
+    },
+
+    {
+      "nvim-neorg/neorg",
+      -- lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+      ft = "norg",
+      version = "*", -- Pin Neorg to the latest stable release
+      dependencies = {
+        { "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" } },
+      },
+      config = function(_, _)
+        local load = {
+          ["core.defaults"] = {},
+
+          ["core.concealer"] = {
+            config = {
+              folds = true,
+              -- icon_preset = 'varied'
+              -- icons = {
+              -- heading = {
+              --   icons = {
+              --     "◉",
+              --     " ◎",
+              --     "  ○",
+              --     "   ✺",
+              --     "    ▶",
+              --     "     ⤷",
+              --   },
+              -- },
+              -- list = {
+              --   icons = {
+              --     " •",
+              --     "  •",
+              --     "   •",
+              --     "    •",
+              --     "     •",
+              --     "      •",
+              --   },
+              -- },
+              -- ordered = {
+              --   icons = {
+              --     "1.",
+              --     " A.",
+              --     "  a.",
+              --     "   (1)",
+              --     "    I.",
+              --     "     i.",
+              --   },
+              -- },
+              -- },
+            },
+          },
+
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = vim.env.HOME .. "/Code/neorg/notes/",
+              },
+            },
+          },
+
+          ["core.integrations.treesitter"] = {},
+
+          ["core.looking-glass"] = {},
+
+          ["core.qol.toc"] = {
+            config = {
+              auto_toc = {
+                open = vim.fn.fnamemodify(vim.fn.expand('%'), ":t") ~= '.dotfiles',
+              },
+            },
+          },
+
+          ["core.ui"] = {},
+
+          ["external.templates"] = {
+            config = {
+              templates_dir = vim.env.HOME .. '/Code/neorg/templates',
+              keywords = {
+                MONDAY_DATE = function() return neorg_templates.weekdate(2) end,
+                TUESDAY_DATE = function() return neorg_templates.weekdate(3) end,
+                WEDNESDAY_DATE = function() return neorg_templates.weekdate(4) end,
+                THURSDAY_DATE = function() return neorg_templates.weekdate(5) end,
+                FRIDAY_DATE = function() return neorg_templates.weekdate(6) end,
+                SATURDAY_DATE = function() return neorg_templates.weekdate(7) end,
+                SUNDAY_DATE = function() return neorg_templates.weekdate(8) end,
+
+                CARRYOVER_TODOS = function() return ls.text_node(neorg_templates.get_carryover_todos()) end,
+
+                INSERT_2 = function() return ls.insert_node(1) end,
+                INSERT_3 = function() return ls.insert_node(1) end,
+                INSERT_4 = function() return ls.insert_node(1) end,
+
+                FILE_NAME_TITLE_CASE = function ()
+                  local name = vim.fn.fnamemodify(vim.fn.expand('%'), ':t:r')
+
+                  return ls.text_node(require('helpers.str_utils').title_case(name))
+                end,
+
+                APPLICATION_STATUS = c(1, { t("applied"), t("rejected") }),
+                TICKET_TYPE = c(1, { t("story"), t("bug"), t("task") }),
+              },
+              snippets_overwrite = {},
+            },
+          },
+        }
+
+        require("neorg").setup({ load = load })
+
+        neorg_templates.template("*/job-search/companies/*.norg", "company")
+        neorg_templates.template("*/journal/*.norg", "daily", ".*/journal/%d%d%d%d/%d%d/w%d%d/%d%d%.norg")
+        neorg_templates.template("*/journal/*index.norg", "weekly", ".*/journal/%d%d%d%d/%d%d/w%d%d/index.norg")
+        neorg_templates.template("*/meetings/*.norg", "meeting", ".*/meetings/%d%d%d%d/.*%.norg")
+        neorg_templates.template("*/meetings/one-on-one/*.norg", "one-on-one", ".*/meetings/one%-on%-one/.*.norg")
+        neorg_templates.template("*/miscellaneous/*.norg", "miscellaneous")
+        neorg_templates.template("*/people/*.norg", "person")
+        neorg_templates.template("*/tickets/*.norg", "ticket")
+        neorg_templates.template("*/aws/courses*.norg", "lesson")
+        neorg_templates.template("*/aws/exam-prep*.norg", "exam-review")
+      end,
+    }
+  }
