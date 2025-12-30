169b436ddb064d5d3cc8b1b55b5b9dab55875517
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-12-30 09:40:42 -0600

    consolidating telescope plugins

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -82,5 +82,20 @@ return {
   {
     "benfowler/telescope-luasnip.nvim",
     module = "telescope._extensions.luasnip",  -- if you wish to lazy-load
+  },
+  {
+    "princejoogie/dir-telescope.nvim",
+    enabled = false,
+    -- telescope.nvim is a required dependency
+    requires = {"nvim-telescope/telescope.nvim"},
+    config = function()
+      require("dir-telescope").setup({
+        -- these are the default options set
+        hidden = false,
+        no_ignore = false,
+        show_preview = true,
+      })
+      require('telescope').load_extension('dir')
+    end,
   }
 }
