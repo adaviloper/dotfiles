f805f6650ad7fd83b587db1ff20ec221c23120d6
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-01-02 12:18:14 -0600

    synchronized cursor style to change in nvim, zsh, and tmux

---
+++ dot_oh-my-zsh/custom/rcfiles/vim.zsh
@@ -0,0 +1,34 @@
+# vi mode
+bindkey -v
+export KEYTIMEOUT=1
+
+# Unbind Esc-h from run-help in vi command mode to prevent conflict with 'h' movement
+# This allows 'h' to work properly as a vi movement key after pressing Escape
+bindkey -M vicmd '\eh' undefined-key
+
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+_fix_cursor() {
+   echo -ne '\e[5 q'
+}
+
+precmd_functions+=(_fix_cursor)
