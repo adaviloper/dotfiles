8bc27a1431f86e645ee4aafb6ae7f652f72fd19f
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-03-17 12:11:53 -0500

    updating pm install script

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Exit if password manager is already installed
-if type op >/dev/null 2>&1; then
+if type pass-cli >/dev/null 2>&1; then
   exit 0
 fi
 
-echo "Installing 1Password CLI."
+echo "Installing Password Manager."
 
 # Detect platform
 case "$(uname -s)" in
@@ -42,18 +42,7 @@ case "$(uname -s)" in
         ubuntu|debian)
           echo "Detected Ubuntu/Debian"
           sudo apt update
-          # 1Password
-          ## Add key for APT repository
-          curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-          ## Add 1Password APT repository
-          echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
-          ## Add debsig-verify policy
-          sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-          curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-          sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-          curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-          # Install 1Password
-          sudo apt update && sudo apt install 1password 1password-cli
+          curl -fsSL https://proton.me/download/pass-cli/install.sh | bash
           ;;
         arch)
           echo "Detected Arch Linux"
