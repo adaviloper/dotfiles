0f1a173a1a817c1a13543002e0a594258c248195
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-05-13 14:57:54 -0500

    setting smart toggle for quitting buffers/windows

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
+local function smart_close()
+  if #vim.api.nvim_tabpage_list_wins(0) > 1 then
+    vim.cmd('quit')
+  else
+    vim.cmd('bdelete')
+  end
+end
+
 vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('adaviloper/close_with_q', { clear = true }),
   desc = 'Close with <q>',
@@ -11,7 +19,16 @@ vim.api.nvim_create_autocmd('FileType', {
     'scratch',
   },
   callback = function(args)
-    vim.keymap.set('n', 'q', '<cmd>quit<cr>', { buffer = args.buf })
+    vim.keymap.set('n', 'q', smart_close, { buffer = args.buf })
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = vim.api.nvim_create_augroup('adaviloper/fugitive_blob_q', { clear = true }),
+  desc = 'Close fugitive blobs with <q> without quitting neovim',
+  pattern = 'fugitive://*',
+  callback = function(args)
+    vim.keymap.set('n', 'q', smart_close, { buffer = args.buf })
   end,
 })
 
@@ -21,6 +38,14 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   group = win_group,
   pattern = { 'help', 'man' },
   command = 'wincmd L',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('adaviloper/fugitiveblame_winbar', { clear = true }),
+  pattern = 'fugitiveblame',
+  callback = function()
+    vim.wo.winbar = ' '
+  end,
 })
 
 local misc_group = vim.api.nvim_create_augroup('adaviloper/miscellaneous', { clear = true })
