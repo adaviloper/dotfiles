f295db6e845d184c2c1c994973bcf933d92242ae
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-05-16 18:29:25 -0500

    updating laravel.nvim config

--- dot_config/nvim/lua/plugins/laravel.lua
+++ dot_config/nvim/lua/plugins/laravel.lua
@@ -5,68 +5,80 @@ return {
   {
     "adalessa/laravel.nvim",
     dependencies = {
-      "tpope/vim-dotenv",
-      "nvim-telescope/telescope.nvim",
       "MunifTanjim/nui.nvim",
-      "kevinhwang91/promise-async",
+      "nvim-lua/plenary.nvim",
+      "nvim-neotest/nvim-nio",
     },
-    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-    -- keys = {
-    --   { "<leader>la", ":Laravel artisan<cr>" },
-    --   { "<leader>lr", ":Laravel routes<cr>" },
-    --   { "<leader>lm", ":Laravel related<cr>" },
-    -- },
+    ft = { "php", "blade" },
+    event = {
+      "BufEnter composer.json",
+    },
+    keys = {
+      { "<F12>ll", function() Laravel.pickers.laravel() end,              desc = "Laravel: Open Laravel Picker" },
+      { "<c-g>",      function() Laravel.commands.run("view:finder") end,    desc = "Laravel: Open View Finder" },
+      { "<F12>la", function() Laravel.commands.run("actions") end,              desc = "Laravel: Open Artisan Picker" },
+      { "<F12>lt", function() Laravel.commands.run('tinker:select') end,        desc = "Laravel: Open Actions Picker" },
+      { "<F12>lr", function() Laravel.pickers.routes() end,               desc = "Laravel: Open Routes Picker" },
+      { "<F12>lh", function() Laravel.run("artisan docs") end,            desc = "Laravel: Open Documentation" },
+      { "<F12>lm", function() Laravel.pickers.make() end,                 desc = "Laravel: Open Make Picker" },
+      { "<F12>lc", function() Laravel.pickers.commands() end,             desc = "Laravel: Open Commands Picker" },
+      { "<F12>lo", function() Laravel.pickers.resources() end,            desc = "Laravel: Open Resources Picker" },
+      { "<F12>lp", function() Laravel.commands.run("command_center") end, desc = "Laravel: Open Command Center" },
+      { "<F12>lu", function() Laravel.commands.run("hub") end,            desc = "Laravel Artisan hub" },
+      {
+        "gf",
+        function()
+          local ok, res = pcall(function()
+            if Laravel.app("gf").cursorOnResource() then
+              return "<cmd>lua Laravel.commands.run('gf')<cr>"
+            end
+          end)
+          if not ok or not res then
+            return "gf"
+          end
+          return res
+        end,
+        expr = true,
+        noremap = true,
+      },
+    },
 
-    opts = function (_, opts)
-      opts.features = {
+    opts = {
+      features = {
+        pickers = {
+          enable = true,
+          provider = "snacks",
+        },
         route_info = {
           enable = true,
           position = "top",
         }
-      }
+      },
 
-      opts.environments = {
+      environments = {
+        ask_on_boot = true,
         definitions = {
           {
             name = "together",
-            condition = {
-              file_exists = { "docker-compose.yml" },
-              executable = { "docker" },
-            },
-            commands = {
-              compose = { "docker", "compose" },
-              {
-                commands = { "php", "composer" },
-                docker = {
-                  container = {
-                    default = "together_api",
-                  },
-                  exec = { "docker", "compose", "exec", },
-                },
-              },
+            map = {
+              php = { "docker", "compose", "exec", "together_api", "php" },
+              composer = { "docker", "compose", "exec", "together_api", "composer" },
             },
           },
           {
             name = "redwood",
-            condition = {
-              file_exists = { "docker-compose.yml" },
-              executable = { "docker" },
-            },
-            commands = {
-              compose = { "docker", "compose" },
-              {
-                commands = { "php", "composer" },
-                docker = {
-                  container = {
-                    default = "redwood_api",
-                  },
-                  exec = { "docker", "compose", "exec", },
-                },
-              },
+            map = {
+              php = { "docker", "compose", "exec", "redwood_api", "php" },
+              composer = { "docker", "compose", "exec", "redwood_api", "composer" },
             },
           },
+          { name = "local" },
         }
-      }
-    end,
+      },
+
+      user_commands = {
+      },
+
+    },
   }
 }
