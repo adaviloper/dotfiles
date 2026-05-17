c749880f5e72434a9888ae5338941255b6bf1cb0
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-05-17 16:14:34 -0500

    automating session file creation

--- dot_oh-my-zsh/custom/rcfiles/functions/directory.zsh
+++ dot_oh-my-zsh/custom/rcfiles/functions/directory.zsh
@@ -2,3 +2,11 @@ mkd () {
   mkdir -p $1
 }
 
+mks () {
+  REPO_URL="$1"
+  REPO_NAME=$(echo "$REPO_URL" | sed "s|.*/||" | sed -E "s|\..*||")
+  # echo "$REPO_NAME"
+  # gcl "$1"
+  touch ~/.local/share/chezmoi/dot_config/tmuxp/${REPO_NAME}.yaml
+}
+
