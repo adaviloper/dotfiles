0e352309387858cefcae3982d060a012b2ab5d3c
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-05-20 14:47:14 -0500

    filter out tmp files from getting saved in the session

--- dot_config/nvim/lua/plugins/resession.lua
+++ dot_config/nvim/lua/plugins/resession.lua
@@ -3,5 +3,22 @@ return {
   'stevearc/resession.nvim',
   opts = {
     dir = utils.RESESSION_ROOT_PATH .. '/' .. vim.fn.getcwd():gsub("^" .. vim.pesc(vim.fn.expand("~")) .. "/?", ""),
+    buf_filter = function (bufnr)
+      local buftype = vim.bo[bufnr].buftype
+      if buftype == "help" then
+        return true
+      end
+      if buftype ~= "" and buftype ~= "acwrite" then
+        return false
+      end
+      local name = vim.api.nvim_buf_get_name(bufnr)
+      if name == "" then
+        return false
+      end
+      if name:match("^/tmp/.*%.md$") then
+        return false
+      end
+      return vim.bo[bufnr].buflisted
+    end
   },
 }
