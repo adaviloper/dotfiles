6416ca0723230ce144d36a2a7e86dbf709d9e72a
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-06-01 23:37:46 -0500

    adding discord and swiftpoint to autostart

--- dot_config/hypr/settings/autostart.lua
+++ dot_config/hypr/settings/autostart.lua
@@ -5,8 +5,10 @@
 
 hl.on("hyprland.start", function()
   hl.exec_cmd(terminal)
+  hl.exec_cmd(chat)
   hl.exec_cmd("nm-applet")
   hl.exec_cmd("waybar & hyprpaper & zen & qs -c noctalia-shell")
+  hl.exec_cmd("bash -c 'sleep 3 && cd ~/.local/opt/swiftpoint-x1 && \"./Swiftpoint X1 Control Panel\"'")
 end)
 
 hl.on("window.active", function()
