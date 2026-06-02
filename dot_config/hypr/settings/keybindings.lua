6091faaf19232ac004b42fb9af1c7fd46856982a
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-06-02 14:46:44 -0500

    updating drag binding

--- dot_config/hypr/settings/keybindings.lua
+++ dot_config/hypr/settings/keybindings.lua
@@ -89,7 +89,7 @@ hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
 hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
-hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind("mouse:274", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Meh binds (Ctrl + Alt + Shift): focus or launch apps
