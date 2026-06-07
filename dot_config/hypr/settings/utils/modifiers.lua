6bbdb999497584c857e57abc8f96ebdb3ba3ee11
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-06-07 18:58:45 -0500

    updating modifier value

--- dot_config/hypr/settings/utils/modifiers.lua
+++ dot_config/hypr/settings/utils/modifiers.lua
@@ -1,5 +1,5 @@
 mainMod = "SUPER" -- Sets "Windows" key as main modifier
-workspaceMod = "SUPER + CONTROL" -- Sets "Windows" key as main modifier
+workspaceMod = "SUPER + ALT" -- Sets "Windows" key as main modifier
 meh = "CONTROL + ALT + SHIFT" -- debug: using ALT only while testing
 hyper = "SUPER + CONTROL + ALT + SHIFT" -- debug: using ALT only while testing
 
