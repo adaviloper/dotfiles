baf921196f739648762605daac9d0892034df3c2
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-06-06 21:10:47 -0500

    adding user bin to path

--- dot_config/hypr/settings/env.lua
+++ dot_config/hypr/settings/env.lua
@@ -1,5 +1,6 @@
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
+hl.env("PATH", os.getenv("HOME") .. "/.config/bin:" .. (os.getenv("PATH") or "/usr/local/bin:/usr/bin:/bin"))
 hl.env("XCURSOR_SIZE", "16")
 hl.env("HYPRCURSOR_SIZE", "16")
 hl.env("QT_QPA_PLATFORMTHEME", "gtk3")
