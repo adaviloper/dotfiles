10f0a875a39508656c69c97588ab1ec940416198
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-06-20 09:27:06 -0500

    setting mouse and updating package list

--- dot_config/hypr/settings/env.lua
+++ dot_config/hypr/settings/env.lua
@@ -1,6 +1,10 @@
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
 hl.env("PATH", os.getenv("HOME") .. "/.config/bin:" .. (os.getenv("PATH") or "/usr/local/bin:/usr/bin:/bin"))
-hl.env("XCURSOR_SIZE", "16")
-hl.env("HYPRCURSOR_SIZE", "16")
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("QT_QPA_PLATFORMTHEME", "gtk3")
+
+hl.env("XCURSOR_THEME", "catppuccin-mocha-lavender-cursors")  -- replace with your color
+hl.env("HYPRCURSOR_THEME", "catppuccin-mocha-lavender-cursors")
+
